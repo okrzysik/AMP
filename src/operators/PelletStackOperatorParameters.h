01919191b5b935921f79e33eb58c53df8b2704d9
Rahul Sampath <sampathrs@ornl.gov>
2011-12-13 17:42:44 -0500

    BugzId: 2396 Parameter class for PelletStackOperator

---
+++ src/operators/PelletStackOperatorParameters.h
@@ -0,0 +1,32 @@
+
+#ifndef included_AMP_PelletStackOperatorParameters
+#define included_AMP_PelletStackOperatorParameters
+
+#include "operators/map/AsyncMapColumnOperator.h"
+
+namespace AMP {
+  namespace Operator {
+
+    class PelletStackOperatorParameters : public OperatorParameters {
+      public :
+
+        PelletStackOperatorParameters(const boost::shared_ptr<AMP::Database>& db)
+          : OperatorParameters(db) {
+            d_currentPellet = static_cast<unsigned int>(-1);
+          }
+
+        virtual ~PelletStackOperatorParameters() { }
+
+        unsigned int d_currentPellet;
+        AMP_MPI d_pelletStackComm;
+        boost::shared_ptr<AMP::Operator::AsyncMapColumnOperator> d_n2nMaps;
+    };
+
+  }  
+}
+
+
+#endif
+
+
+
