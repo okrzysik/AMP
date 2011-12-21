6f17d095c35be92aaf9e243cc97d835b9ee26881
Rahul Sampath <sampathrs@ornl.gov>
2011-12-21 13:22:22 -0500

    More changes for the prototype

--- src/solvers/newFrozenVectorDesign/OnePointSolver.h
+++ src/solvers/newFrozenVectorDesign/OnePointSolver.h
@@ -15,7 +15,15 @@ namespace AMP {
         }
 
         void solve(boost::shared_ptr<AMP::LinearAlgebra::Vector> f, boost::shared_ptr<AMP::LinearAlgebra::Vector> u) {
-
+          AMP::LinearAlgebra::Vector::shared_ptr myU = u->subsetVectorForVariable(d_onePointOp->getOutputVariable());
+          AMP::LinearAlgebra::Vector::shared_ptr myF = f->subsetVectorForVariable(d_onePointOp->getOutputVariable());
+          if(d_bUseZeroInitialGuess) {
+            myU->zero();
+          }
+          AMP::LinearAlgebra::Vector::shared_ptr r = myU->cloneVector();
+          d_onePointOp->apply(f, u, r, -1.0, 1.0);
+          double inverseConstant = 1.0/(d_onePointOp->getConstant());
+          myU->axpy(inverseConstant, r, myU);
         }
 
       protected:
