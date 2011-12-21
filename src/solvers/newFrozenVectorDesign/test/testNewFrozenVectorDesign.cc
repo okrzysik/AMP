b0b0e1eda9f6ef06540e5dd43d5b93de2ae77447
Rahul Sampath <sampathrs@ornl.gov>
2011-12-21 17:52:47 -0500

    minor changes

--- src/solvers/newFrozenVectorDesign/test/testNewFrozenVectorDesign.cc
+++ src/solvers/newFrozenVectorDesign/test/testNewFrozenVectorDesign.cc
@@ -10,6 +10,7 @@
 
 #include "vectors/SimpleVector.h"
 #include "vectors/MultiVector.h"
+#include "vectors/newFrozenVectorDesign/newFrozenVectorDesignHelpers.h"
 
 #include "operators/ColumnOperator.h"
 #include "operators/newFrozenVectorDesign/FirstOperator.h"
@@ -42,6 +43,8 @@ int main(int argc, char *argv[]) {
   columnOp->append(firstOp);
   columnOp->append(secondOp);
 
+  std::cout<<"Constructed Operators."<<std::endl;
+
   boost::shared_ptr<AMP::Database> solver_db = input_db->getDatabase("Solver");
 
   boost::shared_ptr<AMP::Solver::SolverStrategyParameters> firstSolverParams(new AMP::Solver::SolverStrategyParameters(solver_db));
@@ -58,18 +61,30 @@ int main(int argc, char *argv[]) {
   columnSolver->append(firstSolver);
   columnSolver->append(secondSolver);
 
+  std::cout<<"Constructed Solvers."<<std::endl;
+
   AMP::LinearAlgebra::Variable::shared_ptr firstVar = firstOp->getOutputVariable();
   AMP::LinearAlgebra::Variable::shared_ptr secondVar = secondOp->getOutputVariable();
 
-  AMP::LinearAlgebra::Vector::shared_ptr firstVec = AMP::LinearAlgebra::SimpleVector::create(1, firstVar);
-  AMP::LinearAlgebra::Vector::shared_ptr secondVec = AMP::LinearAlgebra::SimpleVector::create(1, secondVar);
+  AMP::LinearAlgebra::Vector::shared_ptr firstVecTmp = AMP::LinearAlgebra::SimpleVector::create(1, firstVar);
+  AMP::LinearAlgebra::Vector::shared_ptr secondVecTmp = AMP::LinearAlgebra::SimpleVector::create(1, secondVar);
+
+  AMP::LinearAlgebra::Vector::shared_ptr firstVec = AMP::LinearAlgebra::MultiVector::view(firstVecTmp, AMP_COMM_SELF);
+  AMP::LinearAlgebra::Vector::shared_ptr secondVec = AMP::LinearAlgebra::MultiVector::view(secondVecTmp, AMP_COMM_SELF);
+
   AMP::LinearAlgebra::Vector::shared_ptr rhsVec = AMP::LinearAlgebra::joinVectors(firstVec, secondVec);
   AMP::LinearAlgebra::Vector::shared_ptr solVec = rhsVec->cloneVector();
+
+  std::cout<<"Constructed Vectors."<<std::endl;
 
   firstVec->setToScalar(3.0);
   secondVec->setToScalar(5.0);
 
+  std::cout<<"Formed RHS."<<std::endl;
+
   columnSolver->solve(rhsVec, solVec);
+
+  std::cout<<"Completed Solve."<<std::endl;
 
   boost::shared_ptr<AMP::LinearAlgebra::SimpleVector> firstSol = 
     boost::dynamic_pointer_cast<AMP::LinearAlgebra::SimpleVector>(solVec->subsetVectorForVariable(firstVar));
--- src/vectors/newFrozenVectorDesign/newFrozenVectorDesignHelpers.cc
+++ src/vectors/newFrozenVectorDesign/newFrozenVectorDesignHelpers.cc
@@ -9,7 +9,7 @@ namespace AMP {
     AMP::LinearAlgebra::Vector::shared_ptr subsetExceptForVariable(AMP::LinearAlgebra::Vector::shared_ptr inVec, 
         AMP::LinearAlgebra::Variable::shared_ptr var) {
       boost::shared_ptr<AMP::LinearAlgebra::MultiVector> outVec = boost::dynamic_pointer_cast<AMP::LinearAlgebra::MultiVector>(
-          AMP::LinearAlgebra::MultiVector::create("MultiVector", AMP_COMM_NULL));
+          AMP::LinearAlgebra::MultiVector::create("MultiVector", AMP_COMM_SELF));
       boost::shared_ptr<AMP::LinearAlgebra::MultiVector> castedInVec = 
         boost::dynamic_pointer_cast<AMP::LinearAlgebra::MultiVector>(inVec);
       AMP::LinearAlgebra::Vector::shared_ptr vec2skip = inVec->subsetVectorForVariable(var);
@@ -27,7 +27,7 @@ namespace AMP {
     AMP::LinearAlgebra::Vector::shared_ptr joinVectors(AMP::LinearAlgebra::Vector::shared_ptr vec1, 
         AMP::LinearAlgebra::Vector::shared_ptr vec2) {
       boost::shared_ptr<AMP::LinearAlgebra::MultiVector> outVec = boost::dynamic_pointer_cast<AMP::LinearAlgebra::MultiVector>(
-          AMP::LinearAlgebra::MultiVector::create("MultiVector", AMP_COMM_NULL));
+          AMP::LinearAlgebra::MultiVector::create("MultiVector", AMP_COMM_SELF));
       outVec->addVector(vec1);
       outVec->addVector(vec2);
       return outVec;
