3eb29037fb60bdf32f361f950d443ac05572d521
Rahul Sampath <sampathrs@ornl.gov>
2012-01-09 17:36:26 -0500

    Converted Nonlinear BVP

--- src/operators/NonlinearBVPOperator.cc
+++ src/operators/NonlinearBVPOperator.cc
@@ -6,109 +6,106 @@
 #include <stdexcept>
 
 namespace AMP {
-namespace Operator {
+  namespace Operator {
 
-  NonlinearBVPOperator :: NonlinearBVPOperator(const boost::shared_ptr<BVPOperatorParameters>& params)
-    : Operator (params) 
-  {
-    AMP_ERROR("NonlinearBVPOperator is not converted yet");
-    /*
-    d_volumeOperator = params->d_volumeOperator;
-    d_boundaryOperator = params->d_boundaryOperator;
-    d_MeshAdapter = d_volumeOperator->getMeshAdapter();
-    */
-  }
-
-  void
-    NonlinearBVPOperator :: apply(const boost::shared_ptr<AMP::LinearAlgebra::Vector> &f, 
-        const boost::shared_ptr<AMP::LinearAlgebra::Vector> &u, boost::shared_ptr<AMP::LinearAlgebra::Vector>&r, double a, double b)
+    NonlinearBVPOperator :: NonlinearBVPOperator(const boost::shared_ptr<BVPOperatorParameters>& params)
+      : Operator (params) 
     {
-      boost::shared_ptr<AMP::LinearAlgebra::Vector> nullRhs;
+      d_volumeOperator = params->d_volumeOperator;
+      d_boundaryOperator = params->d_boundaryOperator;
+      d_Mesh = d_volumeOperator->getMesh();
+    }
 
-      AMP_INSIST( ((r.get()) != NULL), "NULL Residual Vector" );
-
-      AMP::LinearAlgebra::Vector::shared_ptr rInternal = r->subsetVectorForVariable( this->getOutputVariable() );
-
-      AMP_INSIST( ((rInternal.get()) != NULL), "NULL Internal Residual Vector" );
-
-      d_volumeOperator->apply(nullRhs, u, r, 1.0, 0.0);
-
-      if(d_iDebugPrintInfoLevel>3)
+    void
+      NonlinearBVPOperator :: apply(const boost::shared_ptr<AMP::LinearAlgebra::Vector> &f, 
+          const boost::shared_ptr<AMP::LinearAlgebra::Vector> &u, boost::shared_ptr<AMP::LinearAlgebra::Vector>&r, double a, double b)
       {
-        AMP::pout << "L2 Norm of r in NonlinearBVPOperator volumeOperator::apply is : "
-          << r->L2Norm() << std::endl;
-      }
+        boost::shared_ptr<AMP::LinearAlgebra::Vector> nullRhs;
 
-      if(d_iDebugPrintInfoLevel>3)
-      {
-        AMP::pout << "L2 Norm of rInternal in NonlinearBVPOperator volumeOperator::apply is : "
-          << rInternal->L2Norm() << std::endl;
-      }
+        AMP_INSIST( ((r.get()) != NULL), "NULL Residual Vector" );
 
-      d_boundaryOperator->apply(nullRhs, u, r, 1.0, 0.0);
+        AMP::LinearAlgebra::Vector::shared_ptr rInternal = r->subsetVectorForVariable( this->getOutputVariable() );
 
-      if(d_iDebugPrintInfoLevel>3)
-      {
-        AMP::pout << "L2 Norm of r in NonlinearBVPOperator boundaryOperator::apply is : "
-          << r->L2Norm() << std::endl;
-      }
+        AMP_INSIST( ((rInternal.get()) != NULL), "NULL Internal Residual Vector" );
 
-      if(d_iDebugPrintInfoLevel>3)
-      {
-        AMP::pout << "L2 Norm of rInternal in NonlinearBVPOperator boundaryOperator::apply is : "
-          << rInternal->L2Norm() << std::endl;
-      }
+        d_volumeOperator->apply(nullRhs, u, r, 1.0, 0.0);
 
-      if(f.get() == NULL) {
-        rInternal->scale(a);
-      } else {
-        AMP::LinearAlgebra::Vector::shared_ptr fInternal = f->subsetVectorForVariable( this->getOutputVariable() );
-        if(fInternal.get() == NULL) {
+        if(d_iDebugPrintInfoLevel>3)
+        {
+          AMP::pout << "L2 Norm of r in NonlinearBVPOperator volumeOperator::apply is : "
+            << r->L2Norm() << std::endl;
+        }
+
+        if(d_iDebugPrintInfoLevel>3)
+        {
+          AMP::pout << "L2 Norm of rInternal in NonlinearBVPOperator volumeOperator::apply is : "
+            << rInternal->L2Norm() << std::endl;
+        }
+
+        d_boundaryOperator->apply(nullRhs, u, r, 1.0, 0.0);
+
+        if(d_iDebugPrintInfoLevel>3)
+        {
+          AMP::pout << "L2 Norm of r in NonlinearBVPOperator boundaryOperator::apply is : "
+            << r->L2Norm() << std::endl;
+        }
+
+        if(d_iDebugPrintInfoLevel>3)
+        {
+          AMP::pout << "L2 Norm of rInternal in NonlinearBVPOperator boundaryOperator::apply is : "
+            << rInternal->L2Norm() << std::endl;
+        }
+
+        if(f.get() == NULL) {
           rInternal->scale(a);
         } else {
-          rInternal->axpby(b, a, fInternal);
+          AMP::LinearAlgebra::Vector::shared_ptr fInternal = f->subsetVectorForVariable( this->getOutputVariable() );
+          if(fInternal.get() == NULL) {
+            rInternal->scale(a);
+          } else {
+            rInternal->axpby(b, a, fInternal);
+          }
+        }
+
+        if(d_iDebugPrintInfoLevel>2)
+        {
+          AMP::pout << "L2 Norm of output of NonlinearBVPOperator :: apply is : " 
+            << rInternal->L2Norm() << std::endl;
         }
       }
 
-      if(d_iDebugPrintInfoLevel>2)
-      {
-        AMP::pout << "L2 Norm of output of NonlinearBVPOperator :: apply is : " 
-          << rInternal->L2Norm() << std::endl;
-      }
+    void NonlinearBVPOperator :: reset(const boost::shared_ptr<OperatorParameters>& params)
+    {
+      boost::shared_ptr<BVPOperatorParameters> inParams = 
+        boost::dynamic_pointer_cast<BVPOperatorParameters>(params);
+
+      AMP_INSIST( (inParams.get() != NULL), "NonlinearBVPOperator :: reset Null parameter" );
+
+      d_volumeOperator->reset(inParams->d_volumeOperatorParams);
+      d_boundaryOperator->reset(inParams->d_boundaryOperatorParams);
     }
 
-  void NonlinearBVPOperator :: reset(const boost::shared_ptr<OperatorParameters>& params)
-  {
-    boost::shared_ptr<BVPOperatorParameters> inParams = 
-      boost::dynamic_pointer_cast<BVPOperatorParameters>(params);
+    boost::shared_ptr<OperatorParameters> NonlinearBVPOperator :: getJacobianParameters(const boost::shared_ptr<AMP::LinearAlgebra::Vector>& u)
+    {
+      boost::shared_ptr<AMP::Database> db;
+      boost::shared_ptr<BVPOperatorParameters> outParams(new BVPOperatorParameters(db));
 
-    AMP_INSIST( (inParams.get() != NULL), "NonlinearBVPOperator :: reset Null parameter" );
+      outParams->d_volumeOperatorParams = d_volumeOperator->getJacobianParameters(u);
+      outParams->d_boundaryOperatorParams = d_boundaryOperator->getJacobianParameters(u);
 
-    d_volumeOperator->reset(inParams->d_volumeOperatorParams);
-    d_boundaryOperator->reset(inParams->d_boundaryOperatorParams);
+      return outParams;
+    }
+
+    void NonlinearBVPOperator :: modifyRHSvector(AMP::LinearAlgebra::Vector::shared_ptr rhs) {
+      (this->getBoundaryOperator())->addRHScorrection(rhs);
+      (this->getBoundaryOperator())->setRHScorrection(rhs);
+    }
+
+    void NonlinearBVPOperator :: modifyInitialSolutionVector(AMP::LinearAlgebra::Vector::shared_ptr sol) {
+      (this->getBoundaryOperator())->modifyInitialSolutionVector(sol);
+    }
+
   }
-
-  boost::shared_ptr<OperatorParameters> NonlinearBVPOperator :: getJacobianParameters(const boost::shared_ptr<AMP::LinearAlgebra::Vector>& u)
-  {
-    boost::shared_ptr<AMP::Database> db;
-    boost::shared_ptr<BVPOperatorParameters> outParams(new BVPOperatorParameters(db));
-
-    outParams->d_volumeOperatorParams = d_volumeOperator->getJacobianParameters(u);
-    outParams->d_boundaryOperatorParams = d_boundaryOperator->getJacobianParameters(u);
-
-    return outParams;
-  }
-
-  void NonlinearBVPOperator :: modifyRHSvector(AMP::LinearAlgebra::Vector::shared_ptr rhs) {
-    (this->getBoundaryOperator())->addRHScorrection(rhs);
-    (this->getBoundaryOperator())->setRHScorrection(rhs);
-  }
-
-  void NonlinearBVPOperator :: modifyInitialSolutionVector(AMP::LinearAlgebra::Vector::shared_ptr sol) {
-    (this->getBoundaryOperator())->modifyInitialSolutionVector(sol);
-  }
-
-}
 }
 
 
