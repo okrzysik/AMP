8d2ef177741603108071080853cb4ea1d6cc6d4c
Mark Berrill <mbt@lap0086227>
2012-02-01 11:36:37 -0500

    Getting testMatMultiply to compile.  It demonstrates a bug that needs fixing.

--- src/solvers/test/testMatMultiply.cc
+++ src/solvers/test/testMatMultiply.cc
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 
@@ -8,60 +7,81 @@
 #include "utils/Utilities.h"
 #include "utils/ReadTestMesh.h"
 
-#include "ampmesh/MeshVariable.h"
-#include "ampmesh/MeshAdapter.h"
-#include "ampmesh/MeshManager.h"
+#include "ampmesh/Mesh.h"
+#include "discretization/DOF_Manager.h"
+#include "discretization/simpleDOF_Manager.h"
+#include "vectors/Variable.h"
+#include "vectors/Vector.h"
+#include "vectors/VectorBuilder.h"
+#include "matrices/MatrixBuilder.h"
 
+#include "ampmesh/libmesh/initializeLibMesh.h"
+#include "ampmesh/libmesh/libMesh.h"
+
+// libmesh headers
+#include "mesh.h"
+#include "mesh_data.h"
+#include "mesh_generation.h"
+#include "boundary_info.h"
+#include "cell_hex8.h"
 #include "mesh_communication.h"
 
+
 void myTest(AMP::UnitTest *ut, std::string mesh_file) {
-  std::string log_file = "output_testMatMultiply";
+    std::string log_file = "output_testMatMultiply";
+    AMP::PIO::logOnlyNodeZero(log_file);
 
-  AMP::PIO::logOnlyNodeZero(log_file);
+    boost::shared_ptr<AMP::InputDatabase> mesh_file_db(new AMP::InputDatabase("mesh_file_db"));
+    AMP::InputManager::getManager()->parseInputFile(mesh_file, mesh_file_db);
 
-  boost::shared_ptr<AMP::InputDatabase> mesh_file_db(new AMP::InputDatabase("mesh_file_db"));
-  AMP::InputManager::getManager()->parseInputFile(mesh_file, mesh_file_db);
+    // Create a libmesh mesh
+    AMP::AMP_MPI comm(AMP_COMM_SELF);
+    boost::shared_ptr<AMP::Mesh::initializeLibMesh> libmeshInit(new AMP::Mesh::initializeLibMesh(comm));
+    const unsigned int mesh_dim = 3;
+    boost::shared_ptr< ::Mesh > myMesh(new ::Mesh(mesh_dim));
+    AMP::readTestMesh(mesh_file, myMesh);
+    MeshCommunication().broadcast(*(myMesh.get()));
+    myMesh->prepare_for_use(false);
 
-  const unsigned int mesh_dim = 3;
-  boost::shared_ptr< ::Mesh > myMesh(new ::Mesh(mesh_dim));
+    // Create the AMP mesh
+    AMP::Mesh::Mesh::shared_ptr myMeshAdapter( new AMP::Mesh::libMesh(myMesh,"myMesh") );
 
-  AMP::readTestMesh(mesh_file, myMesh);
+    // Create the DOF manager
+    AMP::Discretization::DOFManager::shared_ptr DOFs = AMP::Discretization::simpleDOFManager::create(myMeshAdapter,AMP::Mesh::Vertex,1,3);
 
-  MeshCommunication().broadcast(*(myMesh.get()));
+    // Create the vectors
+    AMP::LinearAlgebra::Variable::shared_ptr myVar (new AMP::LinearAlgebra::Variable("myVar"));
+    AMP::LinearAlgebra::Vector::shared_ptr vec1 = AMP::LinearAlgebra::createVector( DOFs, myVar );
+    vec1->setToScalar(1.0);
 
-  myMesh->prepare_for_use(false);
+    // Create the matrix
+    AMP::LinearAlgebra::Matrix::shared_ptr mat1 = AMP::LinearAlgebra::createMatrix ( vec1, vec1 );
+    mat1->zero();
+    mat1->setDiagonal(vec1);
+    AMP::LinearAlgebra::Matrix::shared_ptr mat2 = mat1->cloneMatrix();
+    mat2->zero();
+    mat2->setDiagonal(vec1);
 
-  AMP::Mesh::MeshManager::Adapter::shared_ptr myMeshAdapter ( new AMP::Mesh::MeshManager::Adapter (myMesh) );
+    AMP::LinearAlgebra::Matrix::shared_ptr mat3 = AMP::LinearAlgebra::Matrix::matMultiply(mat1, mat2);
 
-  AMP::LinearAlgebra::Variable::shared_ptr myVar (new 
-      AMP::LinearAlgebra::VectorVariable<AMP::Mesh::NodalVariable, 3>("myVar", myMeshAdapter));
+    std::vector<unsigned int> cols1;
+    std::vector<double> vals1;
+    mat1->getRowByGlobalID(0, cols1, vals1);
 
-  AMP::LinearAlgebra::Vector::shared_ptr vec1 = myMeshAdapter->createVector(myVar);
-  vec1->setToScalar(1.0);
+    std::vector<unsigned int> cols2;
+    std::vector<double> vals2;
+    mat2->getRowByGlobalID(0, cols2, vals2);
 
-  AMP::LinearAlgebra::Matrix::shared_ptr mat1 = myMeshAdapter->createMatrix(myVar, myVar);
-  mat1->zero();
-  mat1->setDiagonal(vec1);
+    std::vector<unsigned int> cols3;
+    std::vector<double> vals3;
+    mat3->getRowByGlobalID(0, cols3, vals3);
 
-  AMP::LinearAlgebra::Matrix::shared_ptr mat2 = mat1->cloneMatrix();
-  mat2->zero();
-  mat2->setDiagonal(vec1);
+    ut->passes("testMatMultiply");
 
-  AMP::LinearAlgebra::Matrix::shared_ptr mat3 = AMP::LinearAlgebra::Matrix::matMultiply(mat1, mat2);
-
-  std::vector<unsigned int> cols1;
-  std::vector<double> vals1;
-  mat1->getRowByGlobalID(0, cols1, vals1);
-
-  std::vector<unsigned int> cols2;
-  std::vector<double> vals2;
-  mat2->getRowByGlobalID(0, cols2, vals2);
-
-  std::vector<unsigned int> cols3;
-  std::vector<double> vals3;
-  mat3->getRowByGlobalID(0, cols3, vals3);
-
-  ut->passes("testMatMultiply");
+    // Free the mesh in the proper order
+    myMeshAdapter.reset();
+    myMesh.reset();
+    libmeshInit.reset();
 
 }
 
