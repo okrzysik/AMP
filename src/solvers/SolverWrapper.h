dac5dac13973e4bae1b722e854295ce63e57b878
Rahul Sampath <sampathrs@ornl.gov>
2012-02-10 16:39:24 -0500

    adding SolverWrapper

---
+++ src/solvers/SolverWrapper.h
@@ -0,0 +1,39 @@
+
+#ifndef included_SolverWrapper
+#define included_SolverWrapper
+
+#include "solvers/SolverStrategy.h"
+#include "operators/OperatorWrapper.h"
+
+namespace AMP {
+  namespace Solver {
+
+    class SolverWrapper : public SolverStrategy {
+      public:
+        SolverWrapper() { }
+
+        virtual ~SolverWrapper() { }
+
+        void setSolver(boost::shared_ptr<SolverStrategy> in) {
+          d_solver = in;
+        }
+
+        void solve(boost::shared_ptr<AMP::LinearAlgebra::Vector>  f,
+            boost::shared_ptr<AMP::LinearAlgebra::Vector>  u) {
+          boost::shared_ptr<AMP::Operator::OperatorWrapper> wrapOp = boost::dynamic_pointer_cast<
+            AMP::Operator::OperatorWrapper>(d_pOperator);
+          wrapOp->setFullVector(u);
+          AMP::Vector::shared_ptr uTmp = u->subsetVectorForVariable(d_pOperator->getOutputVariable());
+          d_solver->solve(f, uTmp);
+        }
+
+      protected:
+        boost::shared_ptr<SolverStrategy> d_solver;
+    };
+
+  }
+}
+
+#endif
+
+
