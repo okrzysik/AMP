b77a20d4a4a39731cfa587e3026c4b7280e80047
Rahul Sampath <sampathrs@ornl.gov>
2012-02-20 17:07:00 -0500

    Converted testNonlinearMechanics-apply-1

--- src/operators/test/testNonlinearMechanics-apply-1.cc
+++ src/operators/test/testNonlinearMechanics-apply-1.cc
@@ -1,3 +1,4 @@
+
 #include "utils/AMPManager.h"
 #include "utils/UnitTest.h"
 #include "utils/Utilities.h"
@@ -13,7 +14,8 @@
 #include "utils/AMPManager.h"
 #include "utils/PIO.h"
 
-#include "ampmesh/MeshVariable.h"
+#include "discretization/simpleDOF_Manager.h"
+#include "vectors/VectorBuilder.h"
 
 #include "libmesh.h"
 
@@ -36,30 +38,29 @@ void myTest(AMP::UnitTest *ut, std::string exeName)
   input_db->printClassData(AMP::plog);
 
   AMP_INSIST( input_db->keyExists("Mesh"), "Key ''Mesh'' is missing!" );
-  std::string mesh_file = input_db->getString("Mesh");
-
-  AMP::Mesh::MeshManager::Adapter::shared_ptr meshAdapter =
-    AMP::Mesh::MeshManager::Adapter::shared_ptr(new AMP::Mesh::MeshManager::Adapter());
-  meshAdapter->readExodusIIFile ( mesh_file.c_str() );
+  boost::shared_ptr<AMP::Database> mesh_db = input_db->getDatabase("Mesh");
+  boost::shared_ptr<AMP::Mesh::MeshParameters> meshParams(new AMP::Mesh::MeshParameters(mesh_db));
+  meshParams->setComm(AMP::AMP_MPI(AMP_COMM_WORLD));
+  AMP::Mesh::Mesh::shared_ptr meshAdapter = AMP::Mesh::Mesh::buildMesh(meshParams);
 
   AMP_INSIST( input_db->keyExists("testNonlinearMechanicsOperator"), "key missing!" );
 
   boost::shared_ptr<AMP::Operator::ElementPhysicsModel> elementPhysicsModel;
   boost::shared_ptr<AMP::Operator::MechanicsNonlinearFEOperator> testNonlinOperator = 
-    boost::dynamic_pointer_cast<AMP::Operator::MechanicsNonlinearFEOperator>(AMP::Operator::OperatorBuilder::createOperator(meshAdapter,
-															    "testNonlinearMechanicsOperator",
-															    input_db,
-															    elementPhysicsModel));
+    boost::dynamic_pointer_cast<AMP::Operator::MechanicsNonlinearFEOperator>(
+        AMP::Operator::OperatorBuilder::createOperator(meshAdapter,
+          "testNonlinearMechanicsOperator", input_db, elementPhysicsModel));
   testNonlinOperator->init();
 
-  AMP::LinearAlgebra::Variable::shared_ptr solVar = testNonlinOperator->getInputVariable(AMP::Operator::Mechanics::DISPLACEMENT); 
-  AMP::LinearAlgebra::Variable::shared_ptr resVar = testNonlinOperator->getOutputVariable();
+  AMP::LinearAlgebra::Variable::shared_ptr var = testNonlinOperator->getOutputVariable(); 
 
   AMP::LinearAlgebra::Vector::shared_ptr nullVec;
 
-  AMP::LinearAlgebra::Vector::shared_ptr solVec = meshAdapter->createVector( solVar );
-  //AMP::LinearAlgebra::Vector::shared_ptr rhsVec = meshAdapter->createVector( resVar );
-  AMP::LinearAlgebra::Vector::shared_ptr resVec = meshAdapter->createVector( resVar );
+    AMP::Discretization::DOFManager::shared_ptr dofMap = AMP::Discretization::simpleDOFManager::create(
+        meshAdapter, AMP::Mesh::Vertex, 1, 3, true); 
+
+  AMP::LinearAlgebra::Vector::shared_ptr solVec = AMP::LinearAlgebra::createVector(dofMap, var, true);
+  AMP::LinearAlgebra::Vector::shared_ptr resVec = solVec->cloneVector();
 
   solVec->setToScalar(5.0);
 
@@ -67,11 +68,17 @@ void myTest(AMP::UnitTest *ut, std::string exeName)
 
   testNonlinOperator->apply(nullVec, solVec, resVec, 1.0, 0.0);
 
-  AMP::pout<<"resNorm1 = "<<(resVec->L2Norm())<<std::endl;
+  double resNorm1 = resVec->L2Norm();
+
+  AMP::pout<<"resNorm1 = "<<resNorm1<<std::endl;
 
   testNonlinOperator->apply(nullVec, solVec, resVec, 1.0, 0.0);
 
-  AMP::pout<<"resNorm2 = "<<(resVec->L2Norm())<<std::endl;
+  double resNorm2 = resVec->L2Norm();
+
+  AMP::pout<<"resNorm2 = "<<resNorm2<<std::endl;
+
+  AMP_ASSERT(resNorm1 == resNorm2);
 
   ut->passes(exeName);
 
