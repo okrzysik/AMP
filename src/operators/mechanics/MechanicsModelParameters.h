84198f64dc532a10fad014f697b4d844e8f597e1
Rahul Sampath <sampathrs@ornl.gov>
2012-02-27 15:21:20 -0500

    Added some comments

--- src/operators/mechanics/MechanicsModelParameters.h
+++ src/operators/mechanics/MechanicsModelParameters.h
@@ -9,15 +9,21 @@
 namespace AMP {
   namespace Operator {
 
+    /** A class encapsulating all the parameters that the material model
+     * requires to evaluate the stress and/or tangent  */
     class MechanicsModelParameters : public ElementPhysicsModelParameters 
     {
       public :
 
+        /** Constructor */
         MechanicsModelParameters(const boost::shared_ptr<AMP::Database> & db)
           : ElementPhysicsModelParameters(db) { }
 
+        /** Destructor */
         virtual ~MechanicsModelParameters() { }
 
+        /** A vector of deformation gradient values, which are required to
+         * compute the stress and/or tangent. */
         boost::shared_ptr<AMP::LinearAlgebra::Vector> d_deformationGradient;
 
     };
