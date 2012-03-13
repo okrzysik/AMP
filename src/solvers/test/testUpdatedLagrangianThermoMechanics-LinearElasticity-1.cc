4ad5419aff57727821e4f55a9f0bf715a6c6944a
Rahul Sampath <sampathrs@ornl.gov>
2012-03-13 17:02:47 -0400

    testUpdatedLagrangianThermoMechanics-LinearElasticity-1 compiles

--- src/solvers/test/testUpdatedLagrangianThermoMechanics-LinearElasticity-1.cc
+++ src/solvers/test/testUpdatedLagrangianThermoMechanics-LinearElasticity-1.cc
@@ -1,9 +1,13 @@
+
 #include "utils/AMPManager.h"
 #include "utils/InputManager.h"
 #include "utils/UnitTest.h"
 #include "utils/Utilities.h"
-#include "ampmesh/MeshVariable.h"
+
 #include "ampmesh/SiloIO.h"
+
+#include "discretization/simpleDOF_Manager.h"
+#include "vectors/VectorBuilder.h"
 
 #include "operators/mechanics/ThermalStrainMaterialModel.h"
 #include "operators/mechanics/MechanicsLinearFEOperator.h"
@@ -25,18 +29,12 @@
 
 #include "ReadTestMesh.h"
 #include "mesh_communication.h"
-
-
 #include <iostream>
 #include <string>
-
-#include "boost/shared_ptr.hpp"
-
 
 
 void myTest(AMP::UnitTest *ut, std::string exeName) {
   std::string input_file = "input_" + exeName;
-  //std::string output_file = "output_" + exeName + ".txt";
   std::string log_file = "log_" + exeName;
 
   AMP::PIO::logOnlyNodeZero(log_file);
@@ -47,68 +45,50 @@ void myTest(AMP::UnitTest *ut, std::string exeName) {
   AMP::InputManager::getManager()->parseInputFile(input_file, input_db);
   input_db->printClassData(AMP::plog);
 
-  AMP::Mesh::MeshManagerParameters::shared_ptr  meshmgrParams ( new AMP::Mesh::MeshManagerParameters ( input_db ) );
-  AMP::Mesh::MeshManager::shared_ptr  manager ( new AMP::Mesh::MeshManager ( meshmgrParams ) );
-  AMP::Mesh::MeshManager::Adapter::shared_ptr meshAdapter;
-  //meshAdapter = manager->getMesh ( "cylinder" );
-  meshAdapter = manager->getMesh ( "brick" );
-  
-/*  std::string mesh_file = input_db->getString("mesh_file");
-  const unsigned int mesh_dim = 3;
-  boost::shared_ptr< ::Mesh > mesh(new ::Mesh(mesh_dim));
-  AMP::readTestMesh(mesh_file, mesh);
-  MeshCommunication().broadcast(*(mesh.get()));
-  mesh->prepare_for_use(false);
-  AMP::Mesh::MeshManager::Adapter::shared_ptr meshAdapter ( new AMP::Mesh::MeshManager::Adapter (mesh) );
-  manager->addMesh(meshAdapter, "cook");
-*/
+  AMP_INSIST(input_db->keyExists("Mesh"), "Key ''Mesh'' is missing!");
+  boost::shared_ptr<AMP::Database> mesh_db = input_db->getDatabase("Mesh");
+  boost::shared_ptr<AMP::Mesh::MeshParameters> meshParams(new AMP::Mesh::MeshParameters(mesh_db));
+  meshParams->setComm(AMP::AMP_MPI(AMP_COMM_WORLD));
+  AMP::Mesh::Mesh::shared_ptr meshAdapter = AMP::Mesh::Mesh::buildMesh(meshParams);
+
   AMP_INSIST(input_db->keyExists("NumberOfLoadingSteps"), "Key ''NumberOfLoadingSteps'' is missing!");
   int NumberOfLoadingSteps = input_db->getInteger("NumberOfLoadingSteps");
 
-/*  AMP_INSIST(input_db->keyExists("OutputFileName"), "Key ''OutputFileName'' is missing!");
-  std::string outFileName = input_db->getString("OutputFileName");
-
-  FILE* fp; 
-  fp = fopen(outFileName.c_str(), "w");
-  fprintf(fp, "clc; \n clear; \n A = zeros(24, 24); \n \n");
-*/
   //Create a nonlinear BVP operator for mechanics
   AMP_INSIST( input_db->keyExists("NonlinearMechanicsOperator"), "key missing!" );
   boost::shared_ptr<AMP::Operator::ElementPhysicsModel> mechanicsMaterialModel;
-  //boost::shared_ptr<AMP::Database> dirichletVectorCorrectionDatabase = nonlinearMechanicsDatabase->getDatabase("BoundaryOperator");
   boost::shared_ptr<AMP::Operator::NonlinearBVPOperator> nonlinearMechanicsBVPoperator = boost::dynamic_pointer_cast<
-  AMP::Operator::NonlinearBVPOperator>(AMP::Operator::OperatorBuilder::createOperator(meshAdapter,
-										      "NonlinearMechanicsOperator",
-										      input_db,
-										      mechanicsMaterialModel));
+  AMP::Operator::NonlinearBVPOperator>(AMP::Operator::OperatorBuilder::createOperator(
+        meshAdapter, "NonlinearMechanicsOperator", input_db, mechanicsMaterialModel));
 
   //Create the variables
   boost::shared_ptr<AMP::Operator::MechanicsNonlinearFEOperator> mechanicsNonlinearVolumeOperator = 
     boost::dynamic_pointer_cast<AMP::Operator::MechanicsNonlinearFEOperator>(
         nonlinearMechanicsBVPoperator->getVolumeOperator());
-  AMP::LinearAlgebra::Variable::shared_ptr dispVar = mechanicsNonlinearVolumeOperator->getInputVariable(AMP::Operator::Mechanics::DISPLACEMENT);
-  AMP::LinearAlgebra::Variable::shared_ptr tempVar = mechanicsNonlinearVolumeOperator->getInputVariable(AMP::Operator::Mechanics::TEMPERATURE);
-
-  //boost::shared_ptr<AMP::Operator::MechanicsLinearFEOperator> mechanicsLinearVolumeOperator = 
-  //  boost::dynamic_pointer_cast<AMP::Operator::MechanicsLinearFEOperator>(
-  //      linearMechanicsBVPoperator->getVolumeOperator());
+  AMP::LinearAlgebra::Variable::shared_ptr dispVar = mechanicsNonlinearVolumeOperator->getOutputVariable();
+  AMP::LinearAlgebra::Variable::shared_ptr tempVar(new AMP::LinearAlgebra::Variable("temp")); 
 
   //For RHS (Point Forces)
   boost::shared_ptr<AMP::Operator::ElementPhysicsModel> dummyModel;
   boost::shared_ptr<AMP::Operator::DirichletVectorCorrection> dirichletLoadVecOp =
-    boost::dynamic_pointer_cast<AMP::Operator::DirichletVectorCorrection>(AMP::Operator::OperatorBuilder::createOperator(meshAdapter,
-															 "Load_Boundary",
-															 input_db, dummyModel));
+    boost::dynamic_pointer_cast<AMP::Operator::DirichletVectorCorrection>(AMP::Operator::OperatorBuilder::createOperator(
+          meshAdapter, "Load_Boundary", input_db, dummyModel));
   dirichletLoadVecOp->setVariable(dispVar);
+
+  AMP::Discretization::DOFManager::shared_ptr dispDofMap = AMP::Discretization::simpleDOFManager::create(
+      meshAdapter, AMP::Mesh::Vertex, 1, 3, true); 
+
+  AMP::Discretization::DOFManager::shared_ptr tempDofMap = AMP::Discretization::simpleDOFManager::create(
+      meshAdapter, AMP::Mesh::Vertex, 1, 1, true); 
 
   //Create the vectors
   AMP::LinearAlgebra::Vector::shared_ptr nullVec;
-  AMP::LinearAlgebra::Vector::shared_ptr solVec = meshAdapter->createVector( dispVar );
-  AMP::LinearAlgebra::Vector::shared_ptr rhsVec = meshAdapter->createVector( dispVar );
-  AMP::LinearAlgebra::Vector::shared_ptr resVec = meshAdapter->createVector( dispVar );
-  AMP::LinearAlgebra::Vector::shared_ptr scaledRhsVec = meshAdapter->createVector( dispVar );
-  AMP::LinearAlgebra::Vector::shared_ptr refTempVec = meshAdapter->createVector( tempVar );
-  AMP::LinearAlgebra::Vector::shared_ptr curTempVec = meshAdapter->createVector( tempVar );
+  AMP::LinearAlgebra::Vector::shared_ptr solVec = AMP::LinearAlgebra::createVector(dispDofMap, dispVar, true);
+  AMP::LinearAlgebra::Vector::shared_ptr rhsVec = solVec->cloneVector();
+  AMP::LinearAlgebra::Vector::shared_ptr resVec = solVec->cloneVector();
+  AMP::LinearAlgebra::Vector::shared_ptr scaledRhsVec = solVec->cloneVector();
+  AMP::LinearAlgebra::Vector::shared_ptr refTempVec = AMP::LinearAlgebra::createVector(tempDofMap, tempVar, true);
+  AMP::LinearAlgebra::Vector::shared_ptr curTempVec = refTempVec->cloneVector();
 
   //Initial guess
   solVec->zero();
@@ -242,32 +222,9 @@ void myTest(AMP::UnitTest *ut, std::string exeName) {
       nonlinearSolver->setZeroInitialGuess(false);
     }
 
-    //std::cout<<solVec<<std::endl;
-
-/*    boost::shared_ptr<AMP::LinearAlgebra::Matrix> mechMat = linearMechanicsBVPoperator->getMatrix();
-
-    for(int i = 0; i < 24; i++) {
-      std::vector<unsigned int> matCols;
-      std::vector<double> matVals;
-      mechMat->getRowByGlobalID(i, matCols, matVals);
-      for(unsigned int j = 0; j < matCols.size(); j++) {
-        fprintf(fp, "A(%d, %d) = %.15lf ; \n", (i + 1), (matCols[j] + 1), matVals[j]);
-      }//end for j
-      fprintf(fp, "\n");
-    }//end for i
-*/
-/*    char num1[256];
-    sprintf(num1,"%d",step);
-    std::string number1 = num1;
-    std::string fname = exeName + "_Stress_Strain_" + number1 + ".txt";
-    
-    boost::dynamic_pointer_cast<AMP::MechanicsNonlinearFEOperator>(nonlinearMechanicsBVPoperator->getVolumeOperator())->printStressAndStrain(solVec, fname);
-*/    
   }
 
   AMP::pout<<"epsilon = "<<epsilon<<std::endl;
-
-  //mechanicsNonlinearVolumeOperator->printStressAndStrain(solVec, output_file);
 
   ut->passes(exeName);
 }
