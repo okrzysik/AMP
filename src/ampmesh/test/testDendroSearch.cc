2561a16569d656935f3797ed0dea7006db47e70b
Rahul Sampath <sampathrs@ornl.gov>
2012-05-02 18:26:52 -0400

    Initial commit for testDendroSearch

---
+++ src/ampmesh/test/testDendroSearch.cc
@@ -0,0 +1,76 @@
+
+#include "utils/AMPManager.h"
+#include "utils/UnitTest.h"
+#include "utils/Utilities.h"
+#include "utils/Database.h"
+#include "utils/InputDatabase.h"
+#include "utils/InputManager.h"
+#include "utils/AMP_MPI.h"
+#include "utils/PIO.h"
+
+#include "ampmesh/Mesh.h"
+
+#include <iostream>
+#include <string>
+
+#include "mpi.h"
+#include <vector>
+
+#include "sys/sys.h"
+#include "par/parUtils.h"
+#include "oct/TreeNode.h"
+#include "oct/nodeAndValues.h"
+#include "oct/nodeAndRanks.h"
+#include "externVars.h"
+#include "dendro.h"
+
+void myTest(AMP::UnitTest *ut, std::string exeName) {
+  std::string input_file = "input_" + exeName;
+  std::string log_file = "output_" + exeName; 
+
+  ot::RegisterEvents();
+
+  AMP::PIO::logOnlyNodeZero(log_file);
+  AMP::AMP_MPI globalComm(AMP_COMM_WORLD);
+
+  boost::shared_ptr<AMP::InputDatabase> input_db(new AMP::InputDatabase("input_db"));
+  AMP::InputManager::getManager()->parseInputFile(input_file, input_db);
+  input_db->printClassData(AMP::plog);
+
+  AMP_INSIST(input_db->keyExists("Mesh"), "Key ''Mesh'' is missing!");
+  boost::shared_ptr<AMP::Database> mesh_db = input_db->getDatabase("Mesh");
+  boost::shared_ptr<AMP::Mesh::MeshParameters> meshParams(new AMP::Mesh::MeshParameters(mesh_db));
+  meshParams->setComm(AMP::AMP_MPI(AMP_COMM_WORLD));
+  AMP::Mesh::Mesh::shared_ptr meshAdapter = AMP::Mesh::Mesh::buildMesh(meshParams);
+
+
+  ut->passes(exeName);
+}
+
+
+int main(int argc, char *argv[])
+{
+  AMP::AMPManager::startup(argc, argv);
+  AMP::UnitTest ut;
+
+  std::string exeName = "testDendroSearch";
+
+  try {
+    myTest(&ut, exeName);
+  } catch (std::exception &err) {
+    std::cout << "ERROR: While testing "<<argv[0] << err.what() << std::endl;
+    ut.failure("ERROR: While testing");
+  } catch( ... ) {
+    std::cout << "ERROR: While testing "<<argv[0] << "An unknown exception was thrown." << std::endl;
+    ut.failure("ERROR: While testing");
+  }
+
+  ut.report();
+  int num_failed = ut.NumFailGlobal();
+
+  AMP::AMPManager::shutdown();
+  return num_failed;
+}  
+
+
+
