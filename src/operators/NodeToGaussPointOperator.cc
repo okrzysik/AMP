f747a13067d5103613050be99dd2cec65a6acfcb
Rahul Sampath <sampathrs@ornl.gov>
2012-07-05 13:57:14 -0400

    Made NodeToGaussPointOperator work with Volume elements as well

--- src/operators/NodeToGaussPointOperator.cc
+++ src/operators/NodeToGaussPointOperator.cc
@@ -1,3 +1,4 @@
+
 #include "operators/NodeToGaussPointOperator.h"
 /* Libmesh files */
 #include "fe_type.h"
@@ -12,15 +13,16 @@
 #include "string_to_enum.h"
 
 #include "face_quad4.h"
+#include "cell_hex8.h"
 #include "node.h"
 
 namespace AMP {
-namespace Operator {
+  namespace Operator {
 
 
-    void NodeToGaussPointOperator :: apply(const AMP::LinearAlgebra::Vector::shared_ptr &, const AMP::LinearAlgebra::Vector::shared_ptr &u,
-        AMP::LinearAlgebra::Vector::shared_ptr  &r, const double , const double )
-    { 
+    void NodeToGaussPointOperator :: apply(const AMP::LinearAlgebra::Vector::shared_ptr &,
+        const AMP::LinearAlgebra::Vector::shared_ptr &u,
+        AMP::LinearAlgebra::Vector::shared_ptr  &r, const double , const double ) { 
 
       AMP::LinearAlgebra::Vector::shared_ptr nodalVec = u->subsetVectorForVariable(d_NodalVariable);
       AMP::LinearAlgebra::Vector::shared_ptr gaussPtVec = r->subsetVectorForVariable(d_GaussPtVariable);
@@ -35,30 +37,45 @@ namespace Operator {
 
       boost::shared_ptr < ::FEType > d_feType ( new ::FEType(feTypeOrder, feFamily) );
 
+      int dim;
+      if(d_UseSurfaceElements) {
+        dim = 2;
+      } else {
+        dim = 3;
+      }
+
       libMeshEnums::Order qruleOrder = Utility::string_to_enum<libMeshEnums::Order>("SECOND");
-      boost::shared_ptr < ::QBase > d_qrule ( (::QBase::build("QGAUSS", 2, qruleOrder)).release() );
+      boost::shared_ptr < ::QBase > d_qrule ( (::QBase::build("QGAUSS", dim, qruleOrder)).release() );
 
+      AMP::Mesh::MeshIterator el;
+      if(d_UseSurfaceElements) {
+        el = d_Mesh->getIterator(AMP::Mesh::Face, 0);
+      } else {
+        el = d_Mesh->getIterator(AMP::Mesh::Volume, 0);
+      }
 
-      AMP::Mesh::MeshIterator el = d_Mesh->getIterator(AMP::Mesh::Face, 0);
       AMP::Mesh::MeshIterator end_el = el.end();
 
-
       for( ; el != end_el; ++el) {
-        boost::shared_ptr < ::FEBase > d_fe ( (::FEBase::build(2, (*d_feType))).release() );
+        boost::shared_ptr < ::FEBase > d_fe ( (::FEBase::build(dim, (*d_feType))).release() );
         d_fe->attach_quadrature_rule( d_qrule.get() );
-        
+
         std::vector<AMP::Mesh::MeshElement> d_currNodes = el->getElements(AMP::Mesh::Vertex);  
 
-        ::Elem* d_currElemPtr = new ::Quad4;
-        for(size_t j = 0; j < d_currNodes.size(); j++) {
+        ::Elem* d_currElemPtr;
+        if(d_UseSurfaceElements) {
+          d_currElemPtr = new ::Quad4;
+        } else {
+          d_currElemPtr = new ::Hex8;
+        }
+        for(size_t j = 0; j < d_currNodes.size(); ++j) {
           std::vector<double> pt = d_currNodes[j].coord();
           d_currElemPtr->set_node(j) = new ::Node(pt[0], pt[1], pt[2], j);
         }//end for j
 
         std::vector<size_t> bndGlobalIds;
         std::vector<AMP::Mesh::MeshElementID> globalIds(d_currNodes.size()) ;
-        for (size_t i = 0; i<d_currNodes.size(); i++)
-        {
+        for (size_t i = 0; i < d_currNodes.size(); ++i) {
           globalIds[i] = d_currNodes[i].globalID();
         }
         dof_map->getDOFs(globalIds, bndGlobalIds);
@@ -70,27 +87,28 @@ namespace Operator {
         const std::vector<std::vector<Real> > & phi = d_fe->get_phi();
 
         std::vector<double>  computedAtGauss(d_qrule->n_points(), 0.0);
-        for (unsigned int qp = 0; qp < d_qrule->n_points(); qp++){
-          for (unsigned int j = 0; j < bndGlobalIds.size(); j++){
-            double computedAtNode = nodalVec->getValueByGlobalID(bndGlobalIds[j]); 
-            computedAtGauss[qp]     += computedAtNode * phi[j][qp];
-          }//end for j
-        }//end for qp
+        for (unsigned int j = 0; j < bndGlobalIds.size(); ++j){
+          double computedAtNode = nodalVec->getValueByGlobalID(bndGlobalIds[j]); 
+          for (unsigned int qp = 0; qp < d_qrule->n_points(); ++qp){
+            computedAtGauss[qp] += ( computedAtNode * phi[j][qp] );
+          }//end for qp
+        }//end for j
 
-        for (unsigned int qp = 0; qp < d_gaussPtIndices.size(); qp++) {
+        for (unsigned int qp = 0; qp < d_gaussPtIndices.size(); ++qp) {
           gaussPtVec->setLocalValueByGlobalID( d_gaussPtIndices[qp] , computedAtGauss[qp]);
         }
-        for(size_t j = 0; j < d_currElemPtr->n_nodes(); j++) {
+        for(size_t j = 0; j < d_currElemPtr->n_nodes(); ++j) {
           delete (d_currElemPtr->get_node(j));
           d_currElemPtr->set_node(j) = NULL;
         }//end for j
         delete d_currElemPtr;
         d_currElemPtr = NULL;
-
-      }
+      }//end for
 
     }// end apply
 
+  }
 }
-}
+
+
 
