8f371fa0638f8fe82689f93ed40505589b501e8a
Mark Berrill <mbt@lap0086227>
2012-08-01 14:40:36 -0400

    Modifying MeshIterator to inherit from std::iterator (random_access_iterator)

--- src/ampmesh/MeshIterator.h
+++ src/ampmesh/MeshIterator.h
@@ -9,7 +9,16 @@ namespace AMP {
 namespace Mesh {
 
 
-class MeshIterator
+/**
+ * \class MeshIterator
+ * \brief A class used to iterate over elements in a Mesh
+ *
+ * \details  This class provides routines for iterating over a set of elements.
+ *   It is inherited from std::iterator.  The base class provides routines for
+ *   the random access iterators, but does so using the increment/decrement routines.
+ *   Derived classes may (or may not) override these routines for performance optimizations.
+ */
+class MeshIterator : public std::iterator<std::random_access_iterator_tag,AMP::Mesh::MeshElement>
 {
 
 public:
@@ -67,6 +76,45 @@ public:
 
     //! Return the current position (from the beginning) in the iterator
     virtual size_t position() const;
+
+    //! Arithmetic operator+
+    virtual MeshIterator operator+(int) const;
+
+    //! Arithmetic operator+
+    virtual MeshIterator operator+(const MeshIterator&) const;
+
+    //! Arithmetic operator-
+    virtual MeshIterator operator-(int) const;
+
+    //! Arithmetic operator-
+    virtual MeshIterator operator-(const MeshIterator&) const;
+
+    //! Arithmetic operator+=
+    virtual MeshIterator& operator+=(int);
+
+    //! Arithmetic operator+=
+    virtual MeshIterator& operator+=(const MeshIterator&);
+
+    //! Arithmetic operator-=
+    virtual MeshIterator& operator-=(int);
+
+    //! Arithmetic operator-=
+    virtual MeshIterator& operator-=(const MeshIterator&);
+
+    //! Operator <
+    virtual bool operator<(const MeshIterator&);
+
+    //! Operator <=
+    virtual bool operator<=(const MeshIterator&);
+
+    //! Operator >
+    virtual bool operator>(const MeshIterator&);
+
+    //! Operator >=
+    virtual bool operator>=(const MeshIterator&);
+
+    //! Dereference iterator with offset
+    virtual MeshElement& operator[](int);
 
 protected:
     // A pointer to the derived class
