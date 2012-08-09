33d3e43410cf27fd62531b86ec27d8adf07c36a1
Mark Berrill <mbt@lap0086227>
2012-08-09 08:38:14 -0400

    Minor change to build

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -164,13 +164,11 @@ MACRO ( SET_STATIC_FLAGS )
     set(CMAKE_SHARED_LIBRARY_C_FLAGS)         # remove -fPIC
     set(CMAKE_SHARED_LIBRARY_CXX_FLAGS)
     set(CMAKE_SHARED_LINKER_FLAGS)
-    set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS)    # remove -rdynamic
-    set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS)
     # Add the static flag if necessary
     CHECK_ENABLE_FLAG( USE_STATIC 0 )
     IF ( USE_STATIC )
-        SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "-static")    # Add static flag
-        SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "-static")  # Add static flag
+        SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "-static")    # Remove -rdynamic and add static flag
+        SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "-static")  # Remove -rdynamic and add static flag
     ENDIF()
 ENDMACRO()
 
