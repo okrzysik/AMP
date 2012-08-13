a5dc5bd7dc59d7726922b336580e5f3d741696a2
Srikanth Allu <ul2@ornl.gov>
2012-08-13 12:10:47 -0400

    minor changes to Map1to3D map.

--- src/operators/map/Map1Dto3D.cc
+++ src/operators/map/Map1Dto3D.cc
@@ -154,9 +154,11 @@ void Map1Dto3D::computeZGaussLocations(){
           // Get the current position and DOF
           std::vector<Point> coordinates = d_fe->get_xyz();
 
-          t_zLocations.push_back(coordinates[0](2));
-          Xx = coordinates[0](0);
-          Yy = coordinates[0](1);
+          for (unsigned int qp = 0; qp < coordinates.size(); qp++) {
+            t_zLocations.push_back(coordinates[qp](2));
+            Xx = coordinates[qp](0);
+            Yy = coordinates[qp](1);
+          }
           bnd++;
         }
 
@@ -168,9 +170,11 @@ void Map1Dto3D::computeZGaussLocations(){
           d_fe->reinit ( libmeshElements.getElement( bnd->globalID() ));
 
           std::vector<Point> x = d_fe->get_xyz();
-          if( (fabs(Xx-x[0](0)) <= 1.e-12) && (fabs(Yy-x[0](1)) <= 1.e-12) ){ 
-                t_zLocations.push_back(x[0](2));
+          for (unsigned int qp = 0; qp < x.size(); qp++) {
+            if( (fabs(Xx-x[qp](0)) <= 1.e-12) && (fabs(Yy-x[qp](1)) <= 1.e-12) ){ 
+              t_zLocations.push_back(x[qp](2));
             }
+          }
         }
     }
 
