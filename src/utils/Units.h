05d7ef6142a9ea97011163bef23603a2b9fb3ee1
Mark Berrill <mbt@lap0086227>
2013-04-04 10:52:46 -0400

    Adding a units enum file

---
+++ src/utils/Units.h
@@ -0,0 +1,14 @@
+// This file contains a list of enums for units until we have a Units class
+#ifndef included_AMP_Units
+#define included_AMP_Units
+
+namespace AMP {
+namespace Units {
+
+enum Time { seconds, minutes, hours, days };
+enum Length { meters };
+enum Temperature { kelvin };
+
+
+}
+}
