38f715cc5d0c44c2fce20ad4cd273c8ad37c66be
Josh Hansel <jh8@ornl.gov>
2013-06-12 13:49:10 -0400

    changed scaling factors

--- src/operators/subchannel/SubchannelConstants.h
+++ src/operators/subchannel/SubchannelConstants.h
@@ -6,10 +6,10 @@ namespace AMP {
 namespace Operator {
 namespace Subchannel {
 
-const double scaleAxialMassFlowRate = 1e-2;      // Scale the axial mass flow rate by this constant in the vector (controls the norm)
-const double scaleEnthalpy = 1e-3;               // Scale the enthalapy by this constant in the vector (controls the norm)
-const double scalePressure = 1e-2;               // Scale the pressure by this constant in the vector (controls the norm)
-const double scaleLateralMassFlowRate = 1e-2;    // Scale the lateral mass flow rate by this constant in the vector (controls the norm)
+const double scaleAxialMassFlowRate = 6.2e-4;      // Scale the axial mass flow rate by this constant in the vector (controls the norm)
+const double scaleEnthalpy = 2.6e3;               // Scale the enthalapy by this constant in the vector (controls the norm)
+const double scalePressure = 3.1e4;               // Scale the pressure by this constant in the vector (controls the norm)
+const double scaleLateralMassFlowRate = 1.0e-7;    // Scale the lateral mass flow rate by this constant in the vector (controls the norm)
 
 }
 }
