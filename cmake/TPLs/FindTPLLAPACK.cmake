0863b58d324226491f19f82d78987c7edeb0a197
Mark Berrill <mbt@lap0086227.ornl.gov>
2014-04-15 08:59:05 -0400

    Fixing compile error with Tribits

--- cmake/TPLs/FindTPLLAPACK.cmake
+++ cmake/TPLs/FindTPLLAPACK.cmake
@@ -11,3 +11,12 @@ TRIBITS_TPL_DECLARE_LIBRARIES( LAPACK
 # Add the definitions
 SET( USE_EXT_LAPCK 1 )
 
+# Write the blas/lapack header
+SET( BLAS_LAPACK_HEADER ${AMP_INSTALL_DIR}/include/blas_lapack.h )
+CONFIGURE_FILE( ${AMP_SOURCE_DIR}/cmake/BlasLapack/fortran_calls.h ${AMP_INSTALL_DIR}/include/fortran_calls.h COPYONLY )
+FILE(WRITE ${BLAS_LAPACK_HEADER} "// This is a automatically generated file to include blas/lapack headers\n" )
+FILE(APPEND ${BLAS_LAPACK_HEADER} "#ifndef INCLUDE_BLAS_LAPACK\n" )
+FILE(APPEND ${BLAS_LAPACK_HEADER} "#define INCLUDE_BLAS_LAPACK\n" )
+FILE(APPEND ${BLAS_LAPACK_HEADER} "#include \"${AMP_INSTALL_DIR}/include/fortran_calls.h\"\n" )
+FILE(APPEND ${BLAS_LAPACK_HEADER} "#endif\n" )
+
