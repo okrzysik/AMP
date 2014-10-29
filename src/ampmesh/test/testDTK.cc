f3e0802fca59f8e4866d42ba40cb0c394f385466
Damien Lebrun-Grandie <qdi@ornl.gov>
2014-10-29 11:33:50 -0400

    testDTK reads mesh from mechanics

--- src/ampmesh/test/testDTK.cc
+++ src/ampmesh/test/testDTK.cc
@@ -1,14 +1,15 @@
 #include <utils/AMPManager.h>
 #include <utils/UnitTest.h>
 #include <utils/InputDatabase.h>
-
+#include <utils/Writer.h>
+#include <utils/PIO.h>
 #include <ampmesh/mesh.h>
 #include <discretization/simpleDOF_Manager.h>
 #include <vectors/VectorBuilder.h>
 #include <vectors/MultiVariable.h>
 #include <vectors/VectorSelector.h>
 
-#include "Teuchos_RCP.hpp"
+#include <Teuchos_RCP.hpp>
 #include <Teuchos_OpaqueWrapper.hpp>
 #include <Teuchos_DefaultMpiComm.hpp>
 #include <DTK_MeshFreeInterpolator.hpp>
@@ -122,25 +123,83 @@ void applyDTKInterpolator(
   rearrangeData(targetBuffer, data_dim, targetVector);
 }
 
+void read_displacement()
+{
+    // load the source mesh
+    AMP::pout<<"Loading the source mesh"<<std::endl;
+    boost::shared_ptr<AMP::InputDatabase> sourceMeshDatabase(new AMP::InputDatabase("SourceMesh"));
+    sourceMeshDatabase->putString("MeshName", "mesh");
+    sourceMeshDatabase->putString("MeshType", "libMesh");
+    sourceMeshDatabase->putString("FileName", "/Users/qdi/Desktop/read_displacement/layer2_exodus.e");
+    sourceMeshDatabase->putInteger("dim", 3);
+    sourceMeshDatabase->putDouble("x_offset", 0.0);
+    sourceMeshDatabase->putDouble("y_offset", 0.0);
+    sourceMeshDatabase->putDouble("z_offset", 0.0);
+    boost::shared_ptr<AMP::Mesh::MeshParameters> sourceMeshParams(new AMP::Mesh::MeshParameters(sourceMeshDatabase));
+    sourceMeshParams->setComm(AMP::AMP_MPI(AMP_COMM_WORLD));
+    AMP::Mesh::Mesh::shared_ptr sourceMesh = AMP::Mesh::Mesh::buildMesh(sourceMeshParams);
+    AMP::pout<<"***Loading the source mesh"<<std::endl;
+
+    // build source vectors
+    AMP::pout<<"Building the source vector"<<std::endl;
+    bool const split = true;
+    int const ghostWidth = 0;
+    int const dofsPerNode = 3;
+    AMP::Discretization::DOFManager::shared_ptr sourceDofManager = AMP::Discretization::simpleDOFManager::create(sourceMesh, AMP::Mesh::Vertex, ghostWidth, dofsPerNode);
+  AMP::LinearAlgebra::Variable::shared_ptr variable(new AMP::LinearAlgebra::Variable("data"));
+  AMP::LinearAlgebra::Vector::shared_ptr sourceVector = AMP::LinearAlgebra::createVector(sourceDofManager, variable, split);
+
+std::cout<<"HERE"<<std::endl;
+    std::string filename("/Users/qdi/Desktop/read_displacement/displacement");
+    std::ifstream fin(filename.c_str());
+    std::size_t n;
+    fin>>n;
+    ++n; // TODO:  I don't like that
+    std::cout<<"Number of nodes ="<<n;
+    std::vector<double> x(n), y(n), z(n);
+    int node;
+    for (std::size_t i = 0; i < n; ++i) {
+        fin>>node>>x[i]>>y[i]>>z[i];
+        std::cout<<i<<"  "<<node<<"  "<<std::setprecision(15)<<x[i]<<"  "<<y[i]<<"  "<<z[i]<<"\n";
+    } // end for i
+    if (!fin.eof()) std::cerr<<"WARNING:  didn't reach EOF"<<std::endl;
+    fin.close();
+}
+
+
 void testDTK(AMP::UnitTest *ut, std::string exeName) {
+  read_displacement();
+abort();
   // build source and target meshes
-  boost::shared_ptr<AMP::InputDatabase> meshDatabase(new AMP::InputDatabase("Mesh"));
-  meshDatabase->putString("MeshName", "NoName");
-  meshDatabase->putString("MeshType", "AMP");
-  meshDatabase->putInteger("dim", 3);
-  meshDatabase->putDouble("x_offset", 0.0);
-  meshDatabase->putDouble("y_offset", 0.0);
-  meshDatabase->putDouble("z_offset", 0.0);
-  meshDatabase->putString("Generator", "cube");
-  std::vector<int> size(3, 15);
-  meshDatabase->putIntegerArray("Size", size);
-  std::vector<double> range(6, 0.0);
-  range[1] = range[3] = range[5] = 1.0;
-  meshDatabase->putDoubleArray("Range", range);
-  boost::shared_ptr<AMP::Mesh::MeshParameters> meshParams(new AMP::Mesh::MeshParameters(meshDatabase));
-  meshParams->setComm(AMP::AMP_MPI(AMP_COMM_WORLD));
-  AMP::Mesh::Mesh::shared_ptr sourceMesh = AMP::Mesh::Mesh::buildMesh(meshParams);
-  AMP::Mesh::Mesh::shared_ptr targetMesh = AMP::Mesh::Mesh::buildMesh(meshParams);
+  AMP::pout<<"Loading the source mesh"<<std::endl;
+  boost::shared_ptr<AMP::InputDatabase> sourceMeshDatabase(new AMP::InputDatabase("SourceMesh"));
+  sourceMeshDatabase->putString("MeshName", "mesh");
+  sourceMeshDatabase->putString("MeshType", "libMesh");
+  sourceMeshDatabase->putString("FileName", "/Users/qdi/Meshes/coupling.e");
+  sourceMeshDatabase->putInteger("dim", 3);
+  sourceMeshDatabase->putDouble("x_offset", 0.0);
+  sourceMeshDatabase->putDouble("y_offset", 0.0);
+  sourceMeshDatabase->putDouble("z_offset", 0.0);
+  boost::shared_ptr<AMP::Mesh::MeshParameters> sourceMeshParams(new AMP::Mesh::MeshParameters(sourceMeshDatabase));
+  sourceMeshParams->setComm(AMP::AMP_MPI(AMP_COMM_WORLD));
+  AMP::Mesh::Mesh::shared_ptr sourceMesh = AMP::Mesh::Mesh::buildMesh(sourceMeshParams);
+  
+  AMP::pout<<"Building the target mesh"<<std::endl;
+  boost::shared_ptr<AMP::InputDatabase> targetMeshDatabase(new AMP::InputDatabase("TargetMesh"));
+  targetMeshDatabase->putString("MeshName", "mesh");
+  targetMeshDatabase->putString("MeshType", "AMP");
+  targetMeshDatabase->putInteger("dim", 3);
+  targetMeshDatabase->putDouble("x_offset", 0.0);
+  targetMeshDatabase->putDouble("y_offset", 0.0);
+  targetMeshDatabase->putDouble("z_offset", 0.0);
+  targetMeshDatabase->putString("Generator", "cube");
+  std::vector<int> const size(3, 15);
+  targetMeshDatabase->putIntegerArray("Size", size);
+  std::vector<double> const range = sourceMesh->getBoundingBox();
+  targetMeshDatabase->putDoubleArray("Range", range);
+  boost::shared_ptr<AMP::Mesh::MeshParameters> targetMeshParams(new AMP::Mesh::MeshParameters(targetMeshDatabase));
+  targetMeshParams->setComm(AMP::AMP_MPI(AMP_COMM_WORLD));
+  AMP::Mesh::Mesh::shared_ptr targetMesh = AMP::Mesh::Mesh::buildMesh(targetMeshParams);
 
   // build dof managers
   bool const split = true;
@@ -152,18 +211,20 @@ void testDTK(AMP::UnitTest *ut, std::string exeName) {
   // build source and target vectors
   AMP::LinearAlgebra::Variable::shared_ptr variable(new AMP::LinearAlgebra::Variable("data"));
   AMP::LinearAlgebra::Vector::shared_ptr sourceVector = AMP::LinearAlgebra::createVector(sourceDofManager, variable, split);
-  AMP::LinearAlgebra::Vector::shared_ptr targetVector = AMP::LinearAlgebra::createVector(sourceDofManager, variable, split);
+  AMP::LinearAlgebra::Vector::shared_ptr targetVector = AMP::LinearAlgebra::createVector(targetDofManager, variable, split);
   getVerticesCoordinates(sourceMesh->getIterator(AMP::Mesh::Vertex, ghostWidth), sourceDofManager, sourceVector);
 
   // build the interpolator and use it
   Teuchos::RCP<Teuchos::OpaqueWrapper<MPI_Comm> const> commWrapper = Teuchos::opaqueWrapper<MPI_Comm>((sourceMesh->getComm()).getCommunicator());
   Teuchos::RCP<Teuchos::MpiComm<int> const> comm = Teuchos::createMpiComm<int>(commWrapper);
   // TODO: need to actually compute h
-  double const h = (range[1] - range[0]) / static_cast<double>(size[0]);
-  AMP::pout<<"source mesh size h = "<<h<<"\n";
+//double const  h = (range[1] - range[0]) / static_cast<double>(size[0]);
+  double const h = 5.0;
   int const space_dim = 3;
+  AMP::pout<<"source mesh size h = "<<h<<"\n";
   double const radius = 2.0*h;
-  std::string const interpolation_type = "Moving Least Square";
+//  std::string const interpolation_type = "Moving Least Square";
+  std::string const interpolation_type = "Spline";
   std::string const basis_type = "Wendland";
   int const basis_order = 2;
   Teuchos::RCP<DataTransferKit::MeshFreeInterpolator> interpolator =
@@ -179,6 +240,17 @@ void testDTK(AMP::UnitTest *ut, std::string exeName) {
   targetVector->subtract(sourceVector, targetVector);
   AMP::pout<<"error l2 norm = "<<targetVector->L2Norm()<<"\n";
   
+#ifdef USE_EXT_SILO
+  AMP::Utilities::Writer::shared_ptr siloWriter = AMP::Utilities::Writer::buildWriter("silo");
+  siloWriter->setDecomposition(1);
+  siloWriter->registerVector(sourceVector, sourceMesh, AMP::Mesh::Vertex, "vector");
+  siloWriter->writeFile("noname", 0);
+  siloWriter.reset();
+  siloWriter = AMP::Utilities::Writer::buildWriter("silo");
+  siloWriter->setDecomposition(1);
+  siloWriter->registerVector(targetVector, targetMesh, AMP::Mesh::Vertex, "vector");
+  siloWriter->writeFile("noname", 1);
+#endif
 
   ut->passes(exeName);
 }
