da76f4e623ea2e72978c83f0251652e602ddeb2a
stuart_slattery <sslattery617@gmail.com>
2014-12-11 15:04:47 -0500

    adding DTK manager helper class for building objects

---
+++ src/operators/map/dtk/DTKAMPMeshManager.cc
@@ -0,0 +1,51 @@
+
+#include "DTKAMPMeshManager.h"
+#include "DTKAMPMeshEntitySet.h"
+#include "DTKAMPMeshEntityLocalMap.h"
+#include "DTKAMPMeshNodalShapeFunction.h"
+
+#include <DTK_EntitySelector.hpp>
+
+namespace AMP {
+namespace Operator {
+
+
+//---------------------------------------------------------------------------//
+// Constructor.
+DTKAMPMeshManager::DTKAMPMeshManager( 
+    const AMP::shared_ptr<AMP::Mesh::Mesh>& mesh,
+    const AMP::shared_ptr<AMP::Discretization::DOFManager>& dof_manager,
+    const DataTransferKit::EntityType entity_type,
+    const std::function<bool(DataTransferKit::Entity)>& predicate )
+{
+    Teuchos::RCP<DataTransferKit::EntitySelector> entity_selector = 
+	Teuchos::rcp( new DataTransferKit::EntitySelector(entity_type,predicate) );
+
+    Teuchos::RCP<DataTransferKit::EntitySet> entity_set = 
+	Teuchos::rcp( new AMPMeshEntitySet(mesh) );
+    
+    Teuchos::RCP<DataTransferKit::EntityLocalMap> local_map =
+	Teuchos::rcp( new AMPMeshEntityLocalMap() );
+
+    Teuchos::RCP<DataTransferKit::EntityShapeFunction> shape_function =
+	Teuchos::rcp( new AMPMeshNodalShapeFunction(dof_manager) );
+
+    d_function_space = Teuchos::rcp( 
+	new DataTransferKit::FunctionSpace(
+	    entity_set,entity_selector,local_map,shape_function) );
+
+    AMP_ASSERT( Teuchos::nonnull(d_function_space) );
+}
+
+//---------------------------------------------------------------------------//
+// Get the function space over which the mesh and its fields are defined.
+Teuchos::RCP<DataTransferKit::FunctionSpace> 
+DTKAMPMeshManager::functionSpace() const
+{
+    return d_function_space;
+}
+
+//---------------------------------------------------------------------------//
+
+}
+}
---
+++ src/operators/map/dtk/DTKAMPMeshManager.h
@@ -0,0 +1,52 @@
+
+#ifndef included_AMP_DTK_AMPMeshManager
+#define included_AMP_DTK_AMPMeshManager
+
+#include "ampmesh/Mesh.h"
+#include "discretization/DOF_Manager.h"
+
+#include "utils/AMP_MPI.h"
+
+#include <DTK_Types.hpp>
+#include <DTK_FunctionSpace.hpp>
+
+namespace AMP {
+namespace Operator {
+
+
+/**
+  * AMP Mesh manager for DTK.
+*/
+class DTKAMPMeshManager
+{
+public :
+
+    /**
+     * Constructor.
+     */
+    DTKAMPMeshManager(
+	const AMP::shared_ptr<AMP::Mesh::Mesh>& mesh,
+	const AMP::shared_ptr<AMP::Discretization::DOFManager>& dof_manager,
+	const DataTransferKit::EntityType entity_type,
+	const std::function<bool(DataTransferKit::Entity)>& predicate );
+
+    //! Destructor
+    ~DTKAMPMeshManager() { }
+
+    /*!
+     * \brief Get the function space over which the mesh and its fields are
+     * defined. 
+     */
+    Teuchos::RCP<DataTransferKit::FunctionSpace> functionSpace() const;
+
+  private:
+
+    // The function space over which the mesh and its fields are defined.
+    Teuchos::RCP<DataTransferKit::FunctionSpace> d_function_space;
+};
+
+
+}
+}
+
+#endif
