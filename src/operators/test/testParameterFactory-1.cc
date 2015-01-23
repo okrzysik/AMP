ed479552454df2cdc4757da131edfcaf52613994
Mark Berrill <mbt@lap0086227.ornl.gov>
2015-01-23 11:33:05 -0500

    Fixing failing test that should not require libmesh

--- src/operators/test/testParameterFactory-1.cc
+++ src/operators/test/testParameterFactory-1.cc
@@ -19,55 +19,42 @@
 
 void ParameterFactoryTest(AMP::UnitTest *ut)
 {
-  std::string exeName("testParameterFactory-1");
-  std::string input_file = "input_" + exeName;
-  std::string log_file = "output_" + exeName;
+    std::string exeName("testParameterFactory-1");
+    std::string input_file = "input_" + exeName;
+    std::string log_file = "output_" + exeName;
 
-  AMP::PIO::logOnlyNodeZero(log_file);
+    AMP::PIO::logOnlyNodeZero(log_file);
 
-  AMP::shared_ptr<AMP::InputDatabase> input_db(new AMP::InputDatabase("input_db"));
-  AMP::InputManager::getManager()->parseInputFile(input_file, input_db);
-  input_db->printClassData(AMP::plog);
+    AMP::shared_ptr<AMP::InputDatabase> input_db(new AMP::InputDatabase("input_db"));
+    AMP::InputManager::getManager()->parseInputFile(input_file, input_db);
+    input_db->printClassData(AMP::plog);
 
-  AMP_INSIST(input_db->keyExists("Mesh"), "Key ''Mesh'' is missing!");
-  std::string mesh_file = input_db->getString("Mesh");
+    // Get the Mesh database and create the mesh parameters
+    AMP::shared_ptr<AMP::Database> database = input_db->getDatabase( "Mesh" );
+    AMP::shared_ptr<AMP::Mesh::MeshParameters> params(new AMP::Mesh::MeshParameters(database));
+    params->setComm(AMP::AMP_MPI(AMP_COMM_WORLD));
 
-  // Create the mesh parameter object
-  AMP::shared_ptr<AMP::MemoryDatabase> database(new AMP::MemoryDatabase("Mesh"));
-  database->putInteger("dim",3);
-  database->putString("MeshName","mesh");
-  database->putString("MeshType","libMesh");
-  database->putString("FileName",mesh_file);
-  AMP::shared_ptr<AMP::Mesh::MeshParameters> params(new AMP::Mesh::MeshParameters(database));
-  params->setComm(AMP::AMP_MPI(AMP_COMM_WORLD));
+    // Create the meshes from the input database
+    AMP::Mesh::Mesh::shared_ptr mesh = AMP::Mesh::Mesh::buildMesh(params);
 
-  // Create the mesh
-  AMP::Mesh::Mesh::shared_ptr  meshAdapter = AMP::Mesh::Mesh::buildMesh(params);
+    AMP_INSIST(input_db->keyExists("Parameter"), "Key ''Parameter'' is missing!");
+    AMP::shared_ptr<AMP::Database> elemOp_db = input_db->getDatabase("Parameter");
+    AMP::shared_ptr<AMP::Operator::OperatorParameters> operatorParameters = AMP::Operator::ParameterFactory::createParameter(elemOp_db, mesh);
 
-  AMP_INSIST(input_db->keyExists("Parameter"), "Key ''Parameter'' is missing!");
-  AMP::shared_ptr<AMP::Database> elemOp_db = input_db->getDatabase("Parameter");
-  AMP::shared_ptr<AMP::Operator::OperatorParameters> operatorParameters = AMP::Operator::ParameterFactory::createParameter(elemOp_db, meshAdapter);
-
-  if(elemOp_db->getString("name")=="DirichletMatrixCorrection")
-    {
+    if(elemOp_db->getString("name")=="DirichletMatrixCorrection") {
+        AMP::shared_ptr<AMP::Operator::DirichletMatrixCorrectionParameters> operatorParams =
+            AMP::dynamic_pointer_cast<AMP::Operator::DirichletMatrixCorrectionParameters>(operatorParameters);
       
-      AMP::shared_ptr<AMP::Operator::DirichletMatrixCorrectionParameters> operatorParams = AMP::dynamic_pointer_cast<AMP::Operator::DirichletMatrixCorrectionParameters>(operatorParameters);
-      
-      if(operatorParams.get()!=NULL)
-    {
-      ut->passes(exeName.c_str());
-    }
-      else
-    {
-      ut->failure(exeName.c_str());
-    }
-    }
-  else
-    {
-      ut->failure(exeName.c_str());      
+        if(operatorParams.get()!=NULL)
+            ut->passes(exeName.c_str());
+        else
+            ut->failure(exeName.c_str());
+    } else {
+        ut->failure(exeName.c_str());      
     }
 
 }
+
 
 int main(int argc, char *argv[])
 {
