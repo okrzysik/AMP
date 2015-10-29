fe0ddb270dfba8180091e703faab830390f611b1
Mark Berrill <mbt@mbt01.ornl.gov>
2015-10-29 10:13:22 -0400

    Fixing compile error from my last commit

--- src/utils/test/test_LapackWrappers.cc
+++ src/utils/test/test_LapackWrappers.cc
@@ -38,33 +38,6 @@ int main( int, char*[] )
         N_errors += err;
     }
 
-    // Run the tests in parallel to check for parallel bugs
-    printf("\nRunning parallel tests\n");
-    int N_threads = 8;
-    ThreadPool tpool(N_threads);
-    std::vector<double> error(N_threads,0);
-    std::vector<ThreadPool::WorkItem*> work(N_threads,NULL);
-    std::vector<ThreadPool::thread_id_t> id(N_threads);
-    for (size_t i=0; i<sizeof(tests)/sizeof(char*); i++) {
-        double t1 = Utilities::time();
-        for (int j=0; j<N_threads; j++)
-            work[j] = new WorkItemFull<int,const char*,int,double&>(
-                Lapack::run_test, tests[i], N[i], error[j] );
-        tpool.add_work(work);
-        tpool.wait_pool_finished();
-        double t2 = Utilities::time();
-        bool pass = true;
-        for (int j=0; j<N_threads; j++)
-            pass = pass && tpool.getFunctionRet<int>(id[j])==0;
-        if ( !pass ) {
-            printf("Failed parallel test: %s\n",tests[i]);
-            N_errors++;
-        }
-        int us = static_cast<int>(1e6*(t2-t1)/(N[i]*N_threads));
-        printf("%7s:  %5i us\n",tests[i],us);
-    }
-
-
     // Finished
     if ( N_errors==0 )
         std::cout << "\nAll tests passed\n";
