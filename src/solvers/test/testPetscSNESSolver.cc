deacb3a59418c32df7d3aead37e21f07d0270bc0
Bobby Philip <philipb@ornl.gov>
2015-12-04 15:56:22 -0500

    fixing typo

--- src/solvers/test/testPetscSNESSolver.cc
+++ src/solvers/test/testPetscSNESSolver.cc
@@ -12,7 +12,7 @@
 
 #include "ampmesh/Mesh.h"
 #include "vectors/NullVector.h"
-#include "vectors/TemplateVector.h"
+#include "vectors/SimpleVector.h"
 #include "vectors/MultiVector.h"
 #include "operators/NullOperator.h"
 #include "operators/IdentityOperator.h"
@@ -38,7 +38,7 @@ void myTest(AMP::UnitTest *ut, std::string exeName)
     
     // Create the solution and function variables
     AMP::LinearAlgebra::Variable::shared_ptr var(new AMP::LinearAlgebra::Variable("x"));
-    AMP::LinearAlgebra::Vector::shared_ptr u = AMP::LinearAlgebra::TemplateVector<double>::create(10,var,solverComm);
+    AMP::LinearAlgebra::Vector::shared_ptr u = AMP::LinearAlgebra::SimpleVector<double>::create(10,var,solverComm);
     AMP::LinearAlgebra::Vector::shared_ptr f = u->cloneVector();
 
     // Create the operator
