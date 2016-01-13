4eb252c7c210da7df581884b956d9ab1999e1ba8
Bobby Philip <bphilip.kondekeril@gmail.com>
2016-01-13 10:51:50 -0500

    fixing warning

--- src/solvers/GMRESSolver.h
+++ src/solvers/GMRESSolver.h
@@ -129,7 +129,8 @@ private:
     int d_iMaxKrylovDimension; //! maximum dimension of the Krylov subspace before a restart or
                                //! termination happens
 
-    int d_restarts; //! logs number of times the solver is restarted
+    // will uncomment when this is implemented
+    //    int d_restarts; //! logs number of times the solver is restarted
 
     int d_nr; // dimension of the least squares system
 
