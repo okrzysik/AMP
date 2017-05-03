91582e799460f6ff086dd34c0dbfe458895eaef6
Mark Berrill <mbt@mbt01.ornl.gov>
2017-05-03 09:39:49 -0400

    Optimizing some mesh element operations

---
+++ src/ampmesh/MeshElement.cc
@@ -0,0 +1,70 @@
+#include "ampmesh/MeshElement.h"
+
+namespace AMP {
+namespace Mesh {
+
+
+/********************************************************
+* Function to return the centroid of an element         *
+********************************************************/
+void MeshElement::centroid( size_t& N, double* center ) const
+{
+    if ( element != nullptr )
+        return element->centroid(N,center);
+    if ( d_globalID.type() == GeomType::Vertex )
+        return coord(N,center);
+    std::vector<MeshElement> nodes;
+    ( element!=nullptr ? element:this )->getElements( GeomType::Vertex, nodes );
+    AMP_ASSERT( !nodes.empty() );
+    nodes[0].coord( N, center );
+    for ( size_t i = 1; i < nodes.size(); i++ ) {
+        double pos[10];
+        nodes[i].coord( N, pos );
+        for ( size_t j = 0; j < N; j++ )
+            center[j] += pos[j];
+    }
+    for ( size_t j = 0; j < N; j++ )
+        center[j] /= nodes.size();
+}
+
+
+/********************************************************
+* Function to return the coordinates of an element      *
+********************************************************/
+void MeshElement::coord( size_t& N, double* x ) const
+{
+    if ( element == nullptr )
+        AMP_ERROR( "coord is not implimented for the base class (" + elementClass() + ")" );
+    return element->coord( N, x );
+}
+
+
+/********************************************************
+* Return the neighbors/elements                         *
+********************************************************/
+void MeshElement::getElements( const GeomType type, std::vector<MeshElement>& elements ) const
+{
+    if ( element == nullptr )
+        AMP_ERROR( "getElements is not implimented for the base class (" + elementClass() + ")" );
+    element->getElements( type, elements );
+}
+void MeshElement::getElementsID( const GeomType type, std::vector<MeshElementID>& ID ) const
+{
+    if ( element != nullptr )
+        return element->getElementsID( type, ID );
+    std::vector<MeshElement> elements;
+    this->getElements( type, elements );
+    ID.resize( elements.size() );
+    for ( size_t i=0; i<elements.size(); i++)
+        ID[i] = elements[i].globalID();
+}
+void MeshElement::getNeighbors( std::vector<MeshElement::shared_ptr>&neighbors ) const
+{
+    if ( element == nullptr )
+        AMP_ERROR( "getNeighbors is not implimented for the base class (" + elementClass() + ")" );
+    element->getNeighbors( neighbors );
+}
+
+
+} // Mesh namespace
+} // AMP namespace
--- src/ampmesh/MeshElement.h
+++ src/ampmesh/MeshElement.h
@@ -27,109 +27,29 @@ public:
      */
     typedef AMP::shared_ptr<MeshElement> shared_ptr;
 
-
     //! Empty constructor for a MeshElement
     inline MeshElement();
-
 
     //! Copy constructor
     inline MeshElement( const MeshElement & );
 
+    //! Move constructor
+    inline MeshElement( MeshElement && );
 
     //! Assignment operator
     inline MeshElement &operator=( const MeshElement & );
 
+    //! Move assignment operator
+    inline MeshElement &operator=( MeshElement && );
 
     //! Destructor for a MeshElement
     virtual inline ~MeshElement();
-
     
     //! Is the mesh element null
     inline bool isNull() const;
 
 
-public: // Virtual functions
-
-    //! Return the owner rank according to AMP_COMM_WORLD
-    virtual inline unsigned int globalOwnerRank() const;
-
-
-    //! Return the element class
-    virtual inline std::string elementClass() const;
-
-
-    //! Return the elements composing the current element
-    virtual inline std::vector<MeshElement> getElements( const GeomType type ) const;
-
-
-    /**
-     *  Return the elements neighboring the current element.
-     *  One neighbor is returned for each side of the element.
-     *  If the side is on the surface, then it's neighbor is null.
-     *  For Verticies, a list of all verticies that share an element is returned.
-     *  This list is in unsorted order.
-     */
-    virtual inline std::vector<MeshElement::shared_ptr> getNeighbors() const;
-
-
-    //! Return the volume of the current element (does not apply to verticies)
-    virtual inline double volume() const;
-
-
-    //! Return the coordinates of the vertex (only applies to verticies)
-    virtual inline std::vector<double> coord() const;
-
-
-    /**
-     * \brief     Return the coordinate of the vertex
-     * \details   This function returns the coordinates of the vertex
-     *   in the given direction (only applies to verticies).
-     * \param i     The direction requested.  Equivalent to coord()[i]
-     */
-    virtual inline double coord( int i ) const;
-
-
-    /**
-     * \brief     Return the centroid of the element
-     * \details   This function returns the centroid of the element.  The
-     *   centroid is defined as the average of the coordinates of the verticies.
-     *   The centroid of a vertex is the vertex and will return the same result as coord().
-     */
-    virtual inline std::vector<double> centroid() const;
-
-
-    /**
-     * \brief     Return true if the element contains the point
-     * \details   This function checks if the given point is inside or
-     *   within TOL of the given element.  If the current element is a vertex,
-     *   this function checks if the point is with TOL of the vertex.
-     * \param pos   The coordinates of the point to check.
-     * \param TOL   The tolerance to use for the computation.
-     */
-    virtual inline bool containsPoint( const std::vector<double> &pos, double TOL = 1e-12 ) const;
-
-
-    //! Check if the element is on the surface
-    virtual inline bool isOnSurface() const;
-
-    /**
-     * \brief     Check if the current element is on the given boundary
-     * \details   Check if the current element is on the boundary specified by the given id
-     * \param id  The boundary id to check
-     */
-    virtual inline bool isOnBoundary( int id ) const;
-
-
-    /**
-     * \brief     Check if the current element is in the given block
-     * \details   Check if the current element is in the block specified by the given id
-     * \param id  The block id to check
-     */
-    virtual inline bool isInBlock( int id ) const;
-
-
 public: // non-virtual functions
-
 
     //! Function to get a pointer to the raw mesh element (structuredMeshElement, etc.)
     inline MeshElement *getRawElement();
@@ -157,6 +77,106 @@ public: // non-virtual functions
     inline bool operator<=( const MeshElementID &rhs ) const { return d_globalID <= rhs; }
     inline bool operator>=( const MeshElementID &rhs ) const { return d_globalID >= rhs; }
 
+    //! Return the coordinates of the vertex (only applies to verticies)
+    inline std::vector<double> coord() const;
+
+
+    //! Return the elements composing the current element
+    inline std::vector<MeshElement> getElements( const GeomType type ) const;
+
+    /**
+     *  Return the elements neighboring the current element.
+     *  One neighbor is returned for each side of the element.
+     *  If the side is on the surface, then it's neighbor is null.
+     *  For Verticies, a list of all verticies that share an element is returned.
+     *  This list is in unsorted order.
+     */
+    inline std::vector<MeshElement::shared_ptr> getNeighbors() const;
+
+    /**
+     * \brief     Return the coordinate of the vertex
+     * \details   This function returns the coordinates of the vertex
+     *   in the given direction (only applies to verticies).
+     * \param i     The direction requested.  Equivalent to coord()[i]
+     */
+    inline double coord( int i ) const;
+
+    /**
+     * \brief     Return the centroid of the element
+     * \details   This function returns the centroid of the element.  The
+     *   centroid is defined as the average of the coordinates of the verticies.
+     *   The centroid of a vertex is the vertex and will return the same result as coord().
+     */
+    inline std::vector<double> centroid() const;
+
+
+public: // Virtual functions
+
+    //! Return the owner rank according to AMP_COMM_WORLD
+    virtual inline unsigned int globalOwnerRank() const;
+
+    //! Return the element class
+    virtual inline std::string elementClass() const;
+
+    //! Return the volume of the current element (does not apply to verticies)
+    virtual inline double volume() const;
+
+    /**
+     * \brief     Return true if the element contains the point
+     * \details   This function checks if the given point is inside or
+     *   within TOL of the given element.  If the current element is a vertex,
+     *   this function checks if the point is with TOL of the vertex.
+     * \param pos   The coordinates of the point to check.
+     * \param TOL   The tolerance to use for the computation.
+     */
+    virtual inline bool containsPoint( const std::vector<double> &pos, double TOL = 1e-12 ) const;
+
+    //! Check if the element is on the surface
+    virtual inline bool isOnSurface() const;
+
+    /**
+     * \brief     Check if the current element is on the given boundary
+     * \details   Check if the current element is on the boundary specified by the given id
+     * \param id  The boundary id to check
+     */
+    virtual inline bool isOnBoundary( int id ) const;
+
+    /**
+     * \brief     Check if the current element is in the given block
+     * \details   Check if the current element is in the block specified by the given id
+     * \param id  The block id to check
+     */
+    virtual inline bool isInBlock( int id ) const;
+
+
+public: // Advanced functions
+
+    //! Return the elements composing the current element
+    virtual void getElements( const GeomType type, std::vector<MeshElement>& elements ) const;
+
+    //! Return the IDs of the elements composing the current element
+    virtual void getElementsID( const GeomType type, std::vector<MeshElementID>& ID ) const;
+
+    /**
+     *  Return the elements neighboring the current element.
+     *  One neighbor is returned for each side of the element.
+     *  If the side is on the surface, then it's neighbor is null.
+     *  For Verticies, a list of all verticies that share an element is returned.
+     *  This list is in unsorted order.
+     */
+    virtual void getNeighbors( std::vector<MeshElement::shared_ptr>& neighbors ) const;
+
+    //! Return the coordinates of the vertex (only applies to verticies)
+    virtual void coord( size_t& N, double* pos ) const;
+
+    /**
+     * \brief     Return the centroid of the element
+     * \details   This function returns the centroid of the element.  The
+     *   centroid is defined as the average of the coordinates of the verticies.
+     *   The centroid of a vertex is the vertex and will return the same result as coord().
+     */
+    virtual void centroid( size_t& N, double* pos ) const;
+
 
 protected:
     // Unique (per class) ID for identifing the underlying iterator
@@ -168,7 +188,7 @@ protected:
     //! The unique global id of the element
     MeshElementID d_globalID;
 
-    // Clone the iterator
+    // Clone the element
     virtual inline MeshElement *clone() const;
 };
 
