ee4fac7340ebca2fc1c13f5204db0103b81ff3fb
Bobby Philip <bphilip.kondekeril@gmail.com>
2020-08-07 13:44:46 -0600

    Debug

--- src/vectors/testHelpers/VectorTests.cpp
+++ src/vectors/testHelpers/VectorTests.cpp
@@ -218,7 +218,8 @@ void VectorTests::AbsVector( AMP::UnitTest *utils )
     auto vec2 = vec1->cloneVector();
     vec1->setRandomValues(vec1);
     auto min = vec1->min(vec1);
-    vec1->addScalar(vec1, fabs(min), vec1);
+    std::cout << "min value is " << min << std::endl;
+    vec1->addScalar(vec1, fabs(min)+.01, vec1);
     vec2->copyVector( vec1 );
     vec2->scale( -1.0, vec2 );
     vec2->abs( vec2, vec2 );
