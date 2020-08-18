c856935b5280ef6e18c95df175e9102a4a8208c4
Bobby Philip <bphilip.kondekeril@gmail.com>
2020-08-18 14:03:34 -0600

    Add missing files

---
+++ src/vectors/petsc/NativePetscVectorData.cpp
@@ -0,0 +1,208 @@
+#include "AMP/vectors/petsc/NativePetscVectorData.h"
+#include "AMP/vectors/petsc/NativePetscVectorOperations.h"
+
+#include "AMP/vectors/petsc/ManagedPetscVector.h"
+
+#include "petsc.h"
+#include "petsc/private/vecimpl.h"
+#include "petscvec.h"
+
+namespace AMP {
+namespace LinearAlgebra {
+
+NativePetscVectorData::NativePetscVectorData( VectorParameters::shared_ptr in_params )
+    : VectorData(), PetscVector()
+{
+    auto npvParams = std::dynamic_pointer_cast<NativePetscVectorParameters>( in_params );
+    d_petscVec     = npvParams->d_InVec;
+    d_pArray       = nullptr;
+    CommunicationListParameters::shared_ptr params( new CommunicationListParameters() );
+    params->d_comm      = npvParams->d_Comm;
+    params->d_localsize = npvParams->d_localsize;
+    setCommunicationList( std::make_shared<CommunicationList>( params ) );
+    d_bDeleteMe  = npvParams->d_Deleteable;
+}
+
+
+NativePetscVectorData::~NativePetscVectorData()
+{
+    resetArray();
+    if ( d_bDeleteMe )
+        PETSC::vecDestroy( &d_petscVec );
+}
+
+void NativePetscVectorData::putRawData( const double *in )
+{
+    int a, b;
+    VecGetOwnershipRange( d_petscVec, &a, &b );
+    AMP_ASSERT( b - a == (int) getLocalSize() );
+    std::vector<int> offs( b - a );
+    for ( size_t j = 0; j != offs.size(); j++ )
+        offs[j] = a + j;
+    VecSetValues( d_petscVec, offs.size(), offs.data(), in, INSERT_VALUES );
+}
+
+
+void NativePetscVectorData::copyOutRawData( double *out ) const
+{
+    std::copy( getRawDataBlock<double>( 0 ), getRawDataBlock<double>( 0 ) + getLocalSize(), out );
+}
+
+
+void NativePetscVectorData::swapData( VectorData &other )
+{
+    resetArray();
+    auto otherData = dynamic_cast<NativePetscVectorData*>(&other);
+    otherData->resetArray();
+    VecSwap( d_petscVec, otherData->getVec() );
+}
+
+std::shared_ptr<VectorData> NativePetscVectorData::cloneData() const
+{
+    resetArray();
+    Vec new_petscVec;
+    VecDuplicate( d_petscVec, &new_petscVec );
+    auto npvParams            = std::make_shared<NativePetscVectorParameters>( new_petscVec, true );
+    npvParams->d_Comm         = getComm();
+    return std::make_shared<NativePetscVectorData>(npvParams);
+}
+   
+size_t NativePetscVectorData::numberOfDataBlocks() const { return 1; }
+
+
+size_t NativePetscVectorData::sizeOfDataBlock( size_t i ) const
+{
+    if ( i != 0 )
+        return 0;
+    return getLocalSize();
+}
+
+std::shared_ptr<ParameterBase> NativePetscVectorData::getParameters() { return d_pParameters; }
+
+void NativePetscVectorData::resetArray()
+{
+    if ( d_pArray ) {
+        VecRestoreArray( d_petscVec, &d_pArray );
+        d_pArray = 0;
+    }
+}
+
+void NativePetscVectorData::resetArray() const
+{
+    if ( d_pArray ) {
+        VecRestoreArray( d_petscVec, &d_pArray );
+        d_pArray = 0;
+    }
+}
+
+void NativePetscVectorData::setValuesByLocalID( int num, size_t *indices, const double *vals )
+{
+    for ( int i = 0; i != num; i++ )
+        getRawDataBlock<double>()[indices[i]] = vals[i];
+}
+
+
+void NativePetscVectorData::setLocalValuesByGlobalID( int num, size_t *indices, const double *vals )
+{
+    resetArray();
+    if ( sizeof( size_t ) == sizeof( PetscInt ) ) {
+        VecSetValues( d_petscVec, num, (PetscInt *) indices, vals, INSERT_VALUES );
+    } else {
+        AMP_ASSERT( getGlobalSize() < 0x80000000 );
+        std::vector<PetscInt> indices2( num, 0 );
+        for ( int i = 0; i < num; i++ )
+            indices2[i] = (PetscInt) indices[i];
+        VecSetValues( d_petscVec, num, &indices2[0], vals, INSERT_VALUES );
+    }
+}
+
+
+void NativePetscVectorData::addValuesByLocalID( int num, size_t *indices, const double *vals )
+{
+    for ( int i = 0; i != num; i++ )
+        getRawDataBlock<double>()[indices[i]] += vals[i];
+}
+
+
+void NativePetscVectorData::addLocalValuesByGlobalID( int num, size_t *indices, const double *vals )
+{
+    resetArray();
+    if ( sizeof( size_t ) == sizeof( PetscInt ) ) {
+        VecSetValues( d_petscVec, num, (PetscInt *) indices, vals, ::ADD_VALUES );
+    } else {
+        AMP_ASSERT( getGlobalSize() < 0x80000000 );
+        std::vector<PetscInt> indices2( num, 0 );
+        for ( int i = 0; i < num; i++ )
+            indices2[i] = (PetscInt) indices[i];
+        VecSetValues( d_petscVec, num, &indices2[0], vals, ::ADD_VALUES );
+    }
+}
+
+size_t NativePetscVectorData::getLocalSize() const
+{
+    int lsize;
+    VecGetLocalSize( d_petscVec, &lsize );
+    return static_cast<size_t>( lsize );
+}
+
+size_t NativePetscVectorData::getGlobalSize() const
+{
+    int gsize;
+    VecGetSize( d_petscVec, &gsize );
+    return static_cast<size_t>( gsize );
+}
+
+
+void *NativePetscVectorData::getRawDataBlockAsVoid( size_t i )
+{
+    if ( i > 0 )
+        return 0;
+    if ( d_pArray == 0 ) {
+        VecGetArray( d_petscVec, &d_pArray );
+    }
+    return d_pArray;
+}
+
+const void *NativePetscVectorData::getRawDataBlockAsVoid( size_t i ) const
+{
+    if ( i > 0 )
+        return 0;
+    if ( d_pArray == 0 ) {
+        VecGetArray( d_petscVec, &d_pArray );
+    }
+    return d_pArray;
+}
+
+
+void NativePetscVectorData::getLocalValuesByGlobalID( int numVals, size_t *ndx, double *vals ) const
+{
+    if ( numVals == 0 )
+        return;
+    if ( sizeof( size_t ) == sizeof( PetscInt ) ) {
+        VecGetValues( d_petscVec, numVals, (PetscInt *) ndx, vals );
+    } else {
+        AMP_ASSERT( getGlobalSize() < 0x80000000 );
+        std::vector<PetscInt> ndx2( numVals, 0 );
+        for ( int i = 0; i < numVals; i++ )
+            ndx2[i] = (PetscInt) ndx[i];
+        VecGetValues( d_petscVec, numVals, &ndx2[0], vals );
+    }
+}
+
+NativePetscVectorParameters::NativePetscVectorParameters( Vec v, bool deleteable )
+{
+    // Get the communicator from the PETSc vector
+    d_InVec       = v;
+    MPI_Comm comm = d_Comm.getCommunicator(); // Get a MPI_comm object from AMP_MPI to pass to PETSc
+    PetscObjectGetComm( reinterpret_cast<PetscObject>( v ), &comm );
+    if ( comm != d_Comm.getCommunicator() )
+        d_Comm = AMP_MPI( comm );
+    d_Deleteable = deleteable;
+    int lsize;
+    VecGetLocalSize( v, &lsize );
+    d_localsize = (size_t) lsize;
+}
+
+
+} // namespace LinearAlgebra
+} // namespace AMP
---
+++ src/vectors/petsc/NativePetscVectorData.h
@@ -0,0 +1,124 @@
+#ifndef included_AMP_NativePetscVectorData
+#define included_AMP_NativePetscVectorData
+
+#include "AMP/utils/AMP_MPI.h"
+#include "AMP/vectors/data/VectorData.h"
+#include "AMP/vectors/petsc/PetscVector.h"
+
+namespace AMP {
+namespace LinearAlgebra {
+
+
+/*! \struct Vec
+    \brief PETSc vector
+*/
+
+
+/** \class NativePetscVectorParameters
+ * \brief Parameters to set when creating a NativePetscVector
+ */
+class NativePetscVectorParameters : public VectorParameters
+{
+public:
+    //!  The vector to wrap
+    Vec d_InVec;
+
+    //!  The communicator associated with the Vec
+    AMP_MPI d_Comm;
+
+    //!  If true, ~NativePetscVector() will call VecDestroy()
+    bool d_Deleteable;
+
+    //! The number of local entities in the vector
+    size_t d_localsize;
+
+    /** \brief Constructor
+     * \details  This will create NativePetscVectorParameters that can be used
+     *   to construct a NativePetscVector around the given ::Vec.  Note that the
+     *   existing vector must be destroyed once through a call to VecDestroy.
+     *   This can be done be the user or the NativePetscVector based on the deleteable
+     *   flag.
+     * \param[in] v            The vector to wrap
+     * \param[in] deleteable   Do we want ~NativePetscVector() to call VecDestroy() on v
+     */
+    NativePetscVectorParameters( Vec v, bool deleteable );
+};
+
+
+/** \class NativePetscVector
+ * \brief An AMP Vector that uses PETSc for parallel data management, linear algebra,
+ * etc.
+ * \details  This is an AMP wrapper to PETSc.  This is different from ManagedPetscVector
+ * in that this class does not replace calls to Vec*.  Rather, it wraps these calls.
+ * This class is used when PETSc is chosen as the default linear algebra engine.
+ *
+ * This class is not to be used directly, just through base class interfaces.
+ * \see PetscVector
+ * \see ManagedPetscVector
+ */
+class NativePetscVectorData : public VectorData, public PetscVector
+{
+public:
+    /** \brief Construct a wrapper for a PETSc Vec from a set of parameters
+     * \param[in] params The parameters describing the Vec
+     */
+    explicit NativePetscVectorData( VectorParameters::shared_ptr params );
+
+    //! Destructor
+    virtual ~NativePetscVectorData();
+
+    std::string VectorDataName() const override { return "NativePetscVector"; }
+    size_t numberOfDataBlocks() const override;
+    size_t sizeOfDataBlock( size_t i ) const override;
+    void putRawData( const double * ) override;
+    void copyOutRawData( double *out ) const override;
+    size_t getLocalSize() const override;
+    size_t getGlobalSize() const override;
+
+    void setValuesByLocalID( int, size_t *, const double * ) override;
+    void setLocalValuesByGlobalID( int, size_t *, const double * ) override;
+    void addValuesByLocalID( int, size_t *, const double * ) override;
+    void addLocalValuesByGlobalID( int, size_t *, const double * ) override;
+
+    void getLocalValuesByGlobalID( int numVals, size_t *ndx, double *vals ) const override;
+
+    // Return the id of the data
+    uint64_t getDataID() const override
+    {
+        return reinterpret_cast<uint64_t>( getRawDataBlockAsVoid( 0 ) );
+    }
+    void *getRawDataBlockAsVoid( size_t i ) override;
+    const void *getRawDataBlockAsVoid( size_t i ) const override;
+    size_t sizeofDataBlockType( size_t ) const override { return sizeof( double ); }
+    bool isTypeId( size_t hash, size_t ) const override
+    {
+        return hash == typeid( double ).hash_code();
+    }
+    void swapData( VectorData &rhs ) override;
+
+    /** \brief Clone the data
+     */
+    std::shared_ptr<VectorData> cloneData() const override;
+
+    std::shared_ptr<ParameterBase> getParameters();
+
+    // We can always delete a NativePetscVector
+    bool petscHoldsView() const override { return false; }
+
+protected:
+
+    void resetArray();
+    void resetArray() const;
+
+private:
+    friend class NativePetscVectorOperations;
+    std::shared_ptr<VectorParameters> d_pParameters;
+    bool d_bDeleteMe;
+    mutable double *d_pArray; // mutable so that we can cache the value
+};
+
+
+} // namespace LinearAlgebra
+} // namespace AMP
+
+#endif
