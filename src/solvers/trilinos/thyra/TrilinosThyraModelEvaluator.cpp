6e2e8a3198b3704513b88b38a2f9b41a7857b4bc
Bobby Philip <bphilip.kondekeril@gmail.com>
2020-08-18 09:37:57 -0600

    Fix erro

--- src/solvers/trilinos/thyra/TrilinosThyraModelEvaluator.cpp
+++ src/solvers/trilinos/thyra/TrilinosThyraModelEvaluator.cpp
@@ -71,7 +71,7 @@ void TrilinosThyraModelEvaluator::evalModelImpl(
         const_cast<AMP::LinearAlgebra::Vector *>( d_rhs.get() )
             ->makeConsistent( AMP::LinearAlgebra::Vector::ScatterType::CONSISTENT_SET );
     }
-    AMP::pout << "x update status " << x->getUpdateStatus() << std::endl;
+    AMP::pout << "x update status " << (int) x->getUpdateStatus() << std::endl;
     AMP_ASSERT( x->getUpdateStatus() == AMP::LinearAlgebra::Vector::UpdateState::UNCHANGED );
     AMP_ASSERT( d_rhs->getUpdateStatus() == AMP::LinearAlgebra::Vector::UpdateState::UNCHANGED );
 
