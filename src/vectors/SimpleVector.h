773d89341e2bd658b0215b1ae3ce7927a80172e9
Bobby Philip <bphilip.kondekeril@gmail.com>
2020-08-17 21:34:04 -0600

    Fix bug

--- src/vectors/SimpleVector.h
+++ src/vectors/SimpleVector.h
@@ -104,7 +104,7 @@ public:
     void swapData( VectorData &rhs ) override;
     std::shared_ptr<VectorData> cloneData() const override;
 // temporary functions
-    void setComm( AMP_MPI &comm) { d_comm = comm; }
+    void setComm( AMP_MPI comm) { d_comm = comm; }
     void allocateVectorData( size_t localSize, size_t numLocal, size_t numGlobal );
 void setDOFManager( std::shared_ptr<AMP::Discretization::DOFManager> dofManager);
     void setCommunicationList( CommunicationList::shared_ptr comm ) override;
