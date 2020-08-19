684066fe2f9fa7ec3190163eca6efd345faa58b7
Bobby Philip <bphilip.kondekeril@gmail.com>
2020-08-18 23:43:04 -0600

    Fix compile

--- src/vectors/test/test_ArrayVector.h
+++ src/vectors/test/test_ArrayVector.h
@@ -11,8 +11,9 @@ static void testArrayVectorDimensions( std::vector<size_t> &dims, AMP::UnitTest 
     auto vec      = AMP::LinearAlgebra::ArrayVector<T>::create( dims, var );
     auto arrayVec = std::dynamic_pointer_cast<AMP::LinearAlgebra::ArrayVector<T>>( vec );
     AMP_ASSERT( arrayVec.get() != NULL );
-    if ( ( arrayVec->getArray().size() == dims ) &&
-         ( arrayVec->getArray().ndim() == (int) dims.size() ) )
+    auto array = dynamic_cast<ArrayVectorData<T>*>(arrayVec->getVectorData())->getArray();
+    if ( ( array.size() == dims ) &&
+         ( array.ndim() == (int) dims.size() ) )
         ut.passes( "ArrayVector correctly returns dimensions" );
     else
         ut.failure( "ArrayVector does not correctly returns dimensions" );
