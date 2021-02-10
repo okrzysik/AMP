f36db7ccdb44c23b148a1009d6f5344ab3506182
Mark Berrill <berrillma@ornl.gov>
2021-02-10 08:30:40 -0500

    Fixing typo

--- src/utils/string_view.h
+++ src/utils/string_view.h
@@ -20,7 +20,7 @@ public:
     constexpr string_view( const string_view & ) noexcept = default;
     constexpr string_view( const char *s ) : d_data( s ), d_size( 0 )
     {
-        if ( s != nullptr )
+        if ( s == nullptr )
             return;
         while ( s[d_size] )
             d_size++;
