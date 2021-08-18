24f4195a6a0d00346296ca5458083fac24f54f05
Bobby Philip <bphilip@lanl.gov>
2021-08-18 15:23:52 -0600

    Remove static

--- src/utils/KokkosManager.h
+++ src/utils/KokkosManager.h
@@ -1,7 +1,7 @@
 #ifndef included_KokkosManager_H_
 #define included_KokkosManager_H_
 namespace AMP::Utilities {
-static void initializeKokkos( int argc, char **argv );
-static void finalizeKokkos();
+void initializeKokkos( int argc, char **argv );
+void finalizeKokkos();
 } // namespace AMP::Utilities
 #endif
