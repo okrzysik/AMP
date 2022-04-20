a00d3dc933f6e1bc4c0e269aa29b28a3cf6efad5
Bobby Philip <bphilip@lanl.gov>
2022-04-20 17:43:07 -0600

    Cmake cuda mods

--- src/utils/KokkosManager.cpp
+++ src/utils/KokkosManager.cpp
@@ -1,10 +1,9 @@
 #include "AMP/AMP_TPLs.h"
 
 #ifdef AMP_USE_KOKKOS
-    #ifndef USE_CUDA
 
-        #include "AMP/utils/KokkosManager.h"
-        #include <Kokkos_Core.hpp>
+    #include "AMP/utils/KokkosManager.h"
+    #include <Kokkos_Core.hpp>
 
 namespace AMP::Utilities {
 
@@ -13,5 +12,4 @@ void initializeKokkos( int &argc, char **argv ) { Kokkos::initialize( argc, argv
 void finalizeKokkos() { Kokkos::finalize(); }
 
 } // namespace AMP::Utilities
-    #endif
 #endif
