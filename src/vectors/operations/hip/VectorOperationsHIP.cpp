e7dfcdec9b564625ec9eff0a88f561324d730132
Brian Romero <beromer@lanl.gov>
2024-04-30 10:52:00 -0700

    add hip vector operatoions

---
+++ src/vectors/operations/hip/VectorOperationsHIP.cpp
@@ -0,0 +1,7 @@
+#include "AMP/vectors/operations/hip/VectorOperationsHIP.h"
+#include "AMP/vectors/operations/hip/VectorOperationsHIP.hpp"
+
+
+// Explicit instantiations
+template class AMP::LinearAlgebra::VectorOperationsHip<double>;
+template class AMP::LinearAlgebra::VectorOperationsHip<float>;
