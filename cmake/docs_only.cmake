2f60f325c184eaed09a0b7fca9ce5a405a980dfc
Mark Berrill <berrillma@ornl.gov>
2024-05-13 15:00:37 -0400

    Fixing doc only build

---
+++ cmake/docs_only.cmake
@@ -0,0 +1,16 @@
+INCLUDE( "cmake/WriteRepoVersion.cmake" )
+
+# Check if a flag is enabled
+MACRO( CHECK_ENABLE_FLAG FLAG DEFAULT )
+    IF ( NOT DEFINED ${FLAG} )
+        SET( ${FLAG} ${DEFAULT} )
+    ELSEIF ( ${FLAG} STREQUAL "" )
+        SET( ${FLAG} ${DEFAULT} )
+    ELSEIF ( ( ${${FLAG}} STREQUAL "FALSE" ) OR ( ${${FLAG}} STREQUAL "false" ) OR ( ${${FLAG}} STREQUAL "0" ) OR ( ${${FLAG}} STREQUAL "OFF" ) )
+        SET( ${FLAG} 0 )
+    ELSEIF ( ( ${${FLAG}} STREQUAL "TRUE" ) OR ( ${${FLAG}} STREQUAL "true" ) OR ( ${${FLAG}} STREQUAL "1" ) OR ( ${${FLAG}} STREQUAL "ON" ) )
+        SET( ${FLAG} 1 )
+    ELSE()
+        MESSAGE( "Bad value for ${FLAG} (${${FLAG}}); use true or false" )
+    ENDIF()
+ENDMACRO()
