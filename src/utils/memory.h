7f33e994640aaf791efa998bdedd84b8cf59b011
Brian Estevan Romero <beromer@lanl.gov>
2024-08-28 11:25:57 -0600

    Fix createVector

--- src/utils/memory.h
+++ src/utils/memory.h
@@ -1,12 +1,16 @@
 #ifndef included_AMP_MEMORY
 #define included_AMP_MEMORY
 
+#include <type_traits>
+
 #ifdef USE_CUDA
     #include "AMP/utils/cuda/CudaAllocator.h"
 #endif
 #ifdef USE_HIP
     #include "AMP/utils/hip/HipAllocator.h"
 #endif
+#include "AMP/utils/Utilities.h"
+
 
 namespace AMP {
 // managed allocators
@@ -38,6 +42,29 @@ using HostAllocator = AMP::HipHostAllocator<TYPE>;
 template<typename TYPE>
 using HostAllocator = std::allocator<TYPE>;
 #endif
+
+template<typename ALLOC>
+AMP::Utilities::MemoryType getAllocatorMemoryType()
+{
+    using intAllocator = typename std::allocator_traits<ALLOC>::template rebind_alloc<int>;
+    if ( std::is_same<intAllocator, std::allocator<int>>::value ) {
+        return AMP::Utilities::MemoryType::unregistered;
+#ifdef USE_CUDA
+    } else if ( std::is_same<intAllocator, AMP::CudaManagedAllocator<int>>::value ) {
+        return AMP::Utilities::MemoryType::managed;
+    } else if ( std::is_same<intAllocator, AMP::CudaDevAllocator<int>>::value ) {
+        return AMP::Utilities::MemoryType::device;
+#endif
+#ifdef USE_HIP
+    } else if ( std::is_same<intAllocator, AMP::HipManagedAllocator<int>>::value ) {
+        return AMP::Utilities::MemoryType::managed;
+    } else if ( std::is_same<intAllocator, AMP::HipDevAllocator<int>>::value ) {
+        return AMP::Utilities::MemoryType::device;
+#endif
+    } else {
+        AMP_ERROR( "Unknown Allocator" );
+    }
+}
 
 } // namespace AMP
 #endif
--- src/vectors/VectorBuilder.cpp
+++ src/vectors/VectorBuilder.cpp
@@ -4,107 +4,17 @@
 #include "AMP/vectors/MultiVariable.h"
 #include "AMP/vectors/MultiVector.h"
 #include "AMP/vectors/VectorBuilder.hpp"
-#include <iostream>
-
 
 namespace AMP::LinearAlgebra {
-
-
-/********************************************************
- * Vector builder                                        *
- ********************************************************/
-Vector::shared_ptr createVector( std::shared_ptr<AMP::Discretization::DOFManager> DOFs,
-                                 std::shared_ptr<Variable> variable,
-                                 bool split )
-{
-    if ( !DOFs )
-        return Vector::shared_ptr();
-    AMP_ASSERT( variable );
-    // Check if we are dealing with a multiDOFManager
-    std::shared_ptr<AMP::Discretization::multiDOFManager> multiDOF;
-    if ( split )
-        multiDOF = std::dynamic_pointer_cast<AMP::Discretization::multiDOFManager>( DOFs );
-    // Check if we are dealing with a multiVariable
-    auto multiVariable = std::dynamic_pointer_cast<MultiVariable>( variable );
-    if ( multiVariable ) {
-        // We are dealing with a MultiVariable, first check that there are no duplicate or null
-        // variables
-        for ( size_t i = 0; i < multiVariable->numVariables(); i++ ) {
-            auto var1 = multiVariable->getVariable( i );
-            AMP_INSIST( var1,
-                        "Error using a MultiVariable in createVector, NULL variables detected" );
-            for ( size_t j = 0; j < i; j++ ) {
-                auto var2 = multiVariable->getVariable( j );
-                AMP_INSIST(
-                    ( *var1 ) != ( *var2 ),
-                    "Error using a MultiVariable in createVector, duplicate variables detected" );
-            }
-        }
-        // Check that all processors have the same number of variables
-        size_t N_var  = multiVariable->numVariables();
-        size_t N_var0 = DOFs->getComm().bcast( N_var, 0 );
-        AMP_INSIST(
-            N_var == N_var0,
-            "The multivariable has a different number of varaibles on different processors" );
-        // Create the Vector for each variable, then combine
-        std::vector<Vector::shared_ptr> vectors;
-        for ( auto var : *multiVariable )
-            vectors.push_back( createVector( DOFs, var, split ) );
-        // Create the multivector
-        AMP_MPI comm = DOFs->getComm();
-        AMP_ASSERT( !comm.isNull() );
-        comm.barrier();
-        auto multiVector = MultiVector::create( variable, comm );
-        multiVector->addVector( vectors );
-        return multiVector;
-    } else if ( multiDOF ) {
-        // We are dealing with a multiDOFManager and want to split the vector based on the DOF
-        // managers
-        auto subDOFs = multiDOF->getDOFManagers();
-        // Get the vectors for each DOF manager
-        std::vector<Vector::shared_ptr> vectors( subDOFs.size() );
-        for ( size_t i = 0; i < subDOFs.size(); i++ )
-            vectors[i] = createVector( subDOFs[i], variable, split );
-        // Create the multivector
-        AMP_MPI comm = DOFs->getComm();
-        AMP_ASSERT( !comm.isNull() );
-        comm.barrier();
-        auto multiVector = MultiVector::create( variable, comm );
-        multiVector->addVector( vectors );
-        return multiVector;
-    } else {
-        // We are ready to create a single vector
-        // Create the communication list
-        AMP_MPI comm = DOFs->getComm();
-        AMP_ASSERT( !comm.isNull() );
-        comm.barrier();
-        std::shared_ptr<CommunicationList> comm_list;
-        auto remote_DOFs = DOFs->getRemoteDOFs();
-        bool ghosts      = comm.anyReduce( !remote_DOFs.empty() );
-        if ( !ghosts ) {
-            // No need for a communication list
-            comm_list = std::make_shared<CommunicationList>( DOFs->numLocalDOF(), DOFs->getComm() );
-        } else {
-            // Construct the communication list
-            auto params           = std::make_shared<CommunicationListParameters>();
-            params->d_comm        = comm;
-            params->d_localsize   = DOFs->numLocalDOF();
-            params->d_remote_DOFs = remote_DOFs;
-            comm_list             = std::make_shared<CommunicationList>( params );
-        }
-        comm.barrier();
-        // Create the vector
-        auto vector = createSimpleVector<double>( variable, DOFs, comm_list );
-        return vector;
-    }
-    return Vector::shared_ptr();
-}
-
 
 /********************************************************
  *  Explicit instantiations                              *
  ********************************************************/
+
 #define INSTANTIATE_VECTOR( TYPE )                                                                 \
+    template Vector::shared_ptr                                                                    \
+    createVector<TYPE, VectorOperationsDefault<TYPE>, VectorDataDefault<TYPE>>(                    \
+        std::shared_ptr<AMP::Discretization::DOFManager>, std::shared_ptr<Variable>, bool );       \
     template Vector::shared_ptr createSimpleVector<TYPE>( size_t, const std::string & );           \
     template Vector::shared_ptr createSimpleVector<TYPE>( size_t, std::shared_ptr<Variable> );     \
     template Vector::shared_ptr createSimpleVector<TYPE>(                                          \
@@ -119,18 +29,43 @@ Vector::shared_ptr createVector( std::shared_ptr<AMP::Discretization::DOFManager
     template Vector::shared_ptr createArrayVector<TYPE>(                                           \
         const ArraySize &, const ArraySize &, const AMP_MPI &, std::shared_ptr<Variable> );        \
     template Vector::shared_ptr createVectorAdaptor<TYPE>(                                         \
-        const std::string &, std::shared_ptr<AMP::Discretization::DOFManager>, TYPE * );
+        const std::string &, std::shared_ptr<AMP::Discretization::DOFManager>, TYPE * );           \
+    template AMP::LinearAlgebra::Vector::shared_ptr createVector<TYPE>(                            \
+        std::shared_ptr<AMP::Discretization::DOFManager> DOFs,                                     \
+        std::shared_ptr<AMP::LinearAlgebra::Variable> variable,                                    \
+        bool split,                                                                                \
+        AMP::Utilities::MemoryType memType );
 INSTANTIATE_VECTOR( double )
 INSTANTIATE_VECTOR( float )
 
 #ifdef USE_DEVICE
-template Vector::shared_ptr
-    createSimpleVector<double,
-                       VectorOperationsDefault<double>,
-                       VectorDataDefault<double, AMP::ManagedAllocator<double>>>(
-        std::shared_ptr<Variable>,
-        std::shared_ptr<AMP::Discretization::DOFManager>,
-        std::shared_ptr<CommunicationList> );
+    #define INSTANTIATE_VECTOR_DEVICE( TYPE )                                                    \
+        template Vector::shared_ptr                                                              \
+            createSimpleVector<TYPE,                                                             \
+                               VectorOperationsDevice<TYPE>,                                     \
+                               VectorDataDefault<TYPE, AMP::ManagedAllocator<TYPE>>>(            \
+                std::shared_ptr<Variable>,                                                       \
+                std::shared_ptr<AMP::Discretization::DOFManager>,                                \
+                std::shared_ptr<CommunicationList> );                                            \
+        template Vector::shared_ptr                                                              \
+            createSimpleVector<TYPE,                                                             \
+                               VectorOperationsDevice<TYPE>,                                     \
+                               VectorDataDefault<TYPE, AMP::DeviceAllocator<TYPE>>>(             \
+                std::shared_ptr<Variable>,                                                       \
+                std::shared_ptr<AMP::Discretization::DOFManager>,                                \
+                std::shared_ptr<CommunicationList> );                                            \
+        template Vector::shared_ptr                                                              \
+        createVector<TYPE,                                                                       \
+                     VectorOperationsDevice<TYPE>,                                               \
+                     VectorDataDefault<TYPE, AMP::ManagedAllocator<TYPE>>>(                      \
+            std::shared_ptr<AMP::Discretization::DOFManager>, std::shared_ptr<Variable>, bool ); \
+        template Vector::shared_ptr                                                              \
+        createVector<TYPE,                                                                       \
+                     VectorOperationsDevice<TYPE>,                                               \
+                     VectorDataDefault<TYPE, AMP::DeviceAllocator<TYPE>>>(                       \
+            std::shared_ptr<AMP::Discretization::DOFManager>, std::shared_ptr<Variable>, bool );
+INSTANTIATE_VECTOR_DEVICE( float )
+INSTANTIATE_VECTOR_DEVICE( double )
 #endif
 
 } // namespace AMP::LinearAlgebra
