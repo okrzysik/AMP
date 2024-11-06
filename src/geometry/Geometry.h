5c140deb90f6173c9aeac240e909b90ef551ba3b
Ian May <imay@lanl.gov>
2024-11-06 11:49:22 -0800

    Format

--- src/geometry/Geometry.h
+++ src/geometry/Geometry.h
@@ -190,9 +190,9 @@ protected:
     Geometry( int ndim ) : d_physicalDim( ndim ) {}
 
     // Delete copy constructors
-    Geometry( Geometry && )                 = delete;
-    Geometry( const Geometry & )            = default;
-    Geometry &operator=( Geometry && )      = delete;
+    Geometry( Geometry && )      = delete;
+    Geometry( const Geometry & ) = default;
+    Geometry &operator=( Geometry && ) = delete;
     Geometry &operator=( const Geometry & ) = delete;
 
 
