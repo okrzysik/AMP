74bd5a4253f943c3e87de12e698f7c19e57ca0e2
Bobby Philip <bphilip@lanl.gov>
2025-02-03 13:31:06 -0700

    format

--- src/utils/Array.hpp
+++ src/utils/Array.hpp
@@ -1123,7 +1123,7 @@ Array<TYPE, FUN, Allocator> Array<TYPE, FUN, Allocator>::coarsen(
                     for ( size_t j2 = 0; j2 < ratio[1]; j2++ ) {
                         for ( size_t i2 = 0; i2 < ratio[0]; i2++ ) {
                             tmp( i2, j2, k2 ) = operator()(
-                                i1 * ratio[0] + i2, j1 * ratio[1] + j2, k1 * ratio[2] + k2 );
+                                i1 *ratio[0] + i2, j1 * ratio[1] + j2, k1 * ratio[2] + k2 );
                         }
                     }
                 }
