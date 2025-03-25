8caac63d5d68246aa6c788a021c7bcf1e334f318
Bobby Philip <bphilip@lanl.gov>
2025-03-25 15:43:26 -0600

    modify as per Jesus' suggestion

--- src/vectors/operations/device/VectorOperationsDevice.hpp
+++ src/vectors/operations/device/VectorOperationsDevice.hpp
@@ -124,11 +124,11 @@ void VectorOperationsDevice<TYPE>::copy( const VectorData &x, VectorData &y )
         auto N     = y.sizeOfDataBlock( 0 );
         DeviceOperationsHelpers<TYPE>::copy( N, xdata, ydata );
         deviceSynchronize();
+        y.copyGhostValues( x );
     } else {
         // Default to VectorOperationsDefault (on cpu)
         getDefaultOps()->copy( x, y );
     }
-    y.copyGhostValues( x );
 }
 
 template<typename TYPE>
