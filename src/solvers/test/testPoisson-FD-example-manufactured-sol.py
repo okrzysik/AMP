17ab855be5b034d4caf35b209c8d5809867ce6cc
Oliver Krzysik <okrzysik@outlook.com>
2025-06-30 10:12:01 -0600

    Adding FD poisson example

---
+++ src/solvers/test/testPoisson-FD-example-manufactured-sol.py
@@ -0,0 +1,78 @@
+import sympy as sym
+
+
+# Some simple code to get C++ code for exact solution and corresponding source term of Poisson problems in 1D, 2D, and 3D. 
+def main():
+
+    # Just uncomment whichever one you want to use.
+    poisson_1D()
+    poisson_2D()
+    #poisson_3D()
+    
+
+def cxx_print(u, f):
+    print("-----------------")
+    print("exact solution u:")
+    print("-----------------")
+    print( "u = ", sym.cxxcode( sym.simplify(u) ), ";" , sep = "")
+    print("")
+    
+    print("--------------")
+    print("source term f:")
+    print("--------------")
+    print( "f = ", sym.cxxcode( sym.simplify(f) ), ";", sep = "" )
+
+
+
+# eqn in 1D
+def poisson_1D():
+    x = sym.symbols('x')
+
+    # Exact solution
+    u = sym.sin(2 * sym.pi * x) 
+
+    # Differential operator applied to exact solution
+    LU = -sym.diff( sym.diff(u, x), x) 
+    
+    print("\n\n1D Poisson problem:")
+    cxx_print(u, LU)
+
+
+# Rotated anisotropic eqn in 2D
+def poisson_2D():
+    x, y, alpha, beta, gamma, g = sym.symbols('x y alpha beta gamma g')
+
+    # Exact solution
+    u = ( sym.sin(2 * sym.pi * x) * sym.sin(4 * sym.pi * y) )
+
+    # Differential operator applied to exact solution
+    LU = -alpha*sym.diff( sym.diff(u, x), x) \
+         -beta *sym.diff( sym.diff(u, y), y) \
+         -gamma*sym.diff( sym.diff(u, x), y)
+    
+    print("\n\n2D Poisson problem:")
+    cxx_print(u, LU)
+
+
+# 3D equation with anisotropy epsilon in the z direction
+def poisson_3D():
+
+    x, y, z, epsilon = sym.symbols('x y z epsilon')
+
+    # Exact solution
+    u = ( sym.sin(2 * sym.pi * x) * sym.sin(4 * sym.pi * y) * sym.sin(6 * sym.pi * z) )
+
+    # Differential operator applied to exact solution
+    LU = -sym.diff( sym.diff(u, x), x) \
+         -sym.diff( sym.diff(u, y), y) \
+         -epsilon*sym.diff( sym.diff(u, z), z)
+    
+    print("\n\n3D Poisson problem:")
+    cxx_print(u, LU)
+
+
+
+
+# Call the main method!
+if __name__ == "__main__":
+    main()
